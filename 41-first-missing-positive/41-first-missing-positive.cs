58ae0f6ab66ace9af5b903231b4c9302de7b2c62
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-01-18 18:42:41 +0530

    Time: 305 ms (43.35%), Space: 60.9 MB (87.81%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cs
+++ 41-first-missing-positive/41-first-missing-positive.cs
@@ -1,14 +1,64 @@
 public class Solution {
     public int FirstMissingPositive(int[] nums) {
         
-        //return LeetcodeApproach(nums);
-        return FirstMissingPositiveTricky(nums);
+        return LeetcodeApproach(nums);
+        //return FirstMissingPositiveTricky(nums);
         //return Hashing(nums);
     }
     
-    public int LeetcodeApproach(int[] nums)
+    public int LeetcodeApproach(int[] A)
     {
-        return 1;
+        // https://leetcode.com/problems/first-missing-positive/discuss/319270/Explanation-of-crucial-observation-needed-to-deduce-algorithm
+        // https://leetcode.com/problems/first-missing-positive/discuss/17214/Java-simple-solution-with-documentation
+        
+        int n = A.Length;
+        
+        // 1. mark numbers (num < 0) and (num > n) with a special marker number (n+1) 
+        // (we can ignore those because if all number are > n then we'll simply return 1)
+        for (int i = 0; i < A.Length; i++)
+        {
+            if(A[i] <= 0 || A[i] > n)
+            {
+                A[i] = n+1;
+            }
+        }
+        
+        // note: all number in the array are now positive, and on the range 1..n+1
+        
+        // 2. mark each cell appearing in the array, by converting the index for that number to negative
+        for (int i = 0; i < A.Length; i++)
+        {
+            // Get abs value
+            int num = Math.Abs(A[i]);
+            
+            if(num > n)
+            {
+                continue;
+            }
+            
+            // since A[4](5th idx) = 5, O based indexing
+            // -1 for zero index based array (so the number 1 will be at pos 0)
+            num--;
+            
+            // Mark the number as appeared by making it negative
+            if(A[num] > 0)
+            {
+                A[num] = -1 * A[num];
+            }
+        }
+        
+        
+        // 3. find the first cell which isn't negative (doesn't appear in the array)
+        for (int i = 0; i < n; i++) 
+        {
+            if(A[i] >= 0)
+            {
+                return i+1;
+            }
+        }
+        
+        // 4. no positive numbers were found, which means the array contains all numbers 1..n, hence, return n + 1
+        return n+1;
     }
     
     public int FirstMissingPositiveTricky(int[] A)
