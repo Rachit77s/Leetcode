41ed86030e5e24c6d77ad0fab10d2cb654814b76
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-03-19 12:55:17 +0530

    Time: 84 ms (90.64%), Space: 41.1 MB (6.79%) - LeetHub

---
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cs
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseKGroup(ListNode head, int k) {
+     
+        /*
+            Traverse linked list from left to right, during traverse, group nodes in k, 
+            then reverse each group.
+        */
+        
+        return ReverseKOne(head, k);
+    }
+    
+    public ListNode ReverseKOne(ListNode head, int k) 
+    {
+        ListNode iterator = head;
+        
+        for(int i = 0; i < k; i++)
+        {
+            if(iterator == null)
+                return head;
+            
+            iterator = iterator.next;
+        }
+        
+        // [1,2,3,4,5,6,7,8], k = 3
+        // head = 1     4   7
+        // next = 4     7
+        // prev = 3     6   8
+        
+        ListNode prev = null;
+        ListNode next = null;
+        ListNode curr = head;
+        
+        int count = 0;
+        
+        while(curr != null && count < k)
+        {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            count++;    
+        }
+        
+        // Next would be pointer to K+1th node
+        if(next != null)
+            head.next = ReverseKOne(next, k);
+        
+        return prev;
+    }
+}
