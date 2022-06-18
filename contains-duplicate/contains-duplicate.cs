98315a734248b0f506115d589ae951db5e2d2733
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-06-19 00:26:55 +0530

    Time: 227 ms, Memory: 46.7 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        HashSet<int> set = new HashSet<int>();
+            
+        for(int i=0; i<nums.Length; i++)
+        {
+            if(set.Contains(nums[i])) {
+                return true;
+            }
+            set.Add(nums[i]);
+           
+        }
+            
+        return false;
+    }
+}
