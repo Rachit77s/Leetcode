21d4f94ee4e23db7661aa08ba4407fb48ee5e2e8
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-07-21 01:10:52 +0530

    Time: 0 ms, Memory: 42.5 MB - LeetHub

--- plus-one/plus-one.java
+++ plus-one/plus-one.java
@@ -20,27 +20,6 @@ class Solution {
         int[] newNumber = new int [n+1];
         newNumber[0] = 1;
         
-        return newNumber;
-        
-        
-//         int n = digits.length;
-//         for(int i=n-1; i>=0; i--) 
-//         {          
-//             if(digits[i] < 9) 
-//             {
-//                 digits[i]++;
-//                 return digits;
-//             }
-
-//             digits[i] = 0;
-//         }
-
-//         int[] newNumber = new int [n+1];
-//         newNumber[0] = 1;
-        
-        
-        
-        
-        
+        return newNumber;        
     }
 }
