8792f7777c4c6cd6efac38d7f7f8a07cfaf9d077
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-07-24 02:13:10 +0530

    Time: 12 ms (76.44%), Space: 42.7 MB (91.14%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,8 +1,40 @@
 class Solution {
     public int firstUniqChar(String s) {
         
+        return OptimizedApproach(s);
+        //return BetterApproach(s);
+    }
+    
+    public int OptimizedApproach(String A)
+    {
+        int[] countArr = new int[26];
+        HashSet<Character> set = new HashSet<>();
         
-        return BetterApproach(s);
+        for(int i = 0; i < 26; i++)
+            countArr[i] = -1;
+        
+        for(int i = 0; i < A.length(); i++)
+        {
+            char ch = A.charAt(i);
+            
+            if(set.contains(ch))
+                countArr[ch - 'a'] = -1;
+            else
+            {
+                set.add(ch);
+                countArr[ch - 'a'] = i;
+            }
+                
+        }
+        
+        int minIndex = Integer.MAX_VALUE;
+        for(int i = 0; i < 26; i++) 
+        {    
+            if(countArr[i] != -1) 
+                minIndex = Math.min(minIndex, countArr[i]);
+        } 
+        
+        return minIndex == Integer.MAX_VALUE ? -1 : minIndex;
     }
     
     public int BetterApproach(String A)
