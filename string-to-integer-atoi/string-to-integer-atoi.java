fcd5a57d8a684d769cbbcbd02f5054d12420d1f2
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-07-28 01:38:01 +0530

    Time: 1 ms, Memory: 42.1 MB - LeetHub

---
+++ string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,113 @@
+class Solution {
+    public int myAtoi(String s) {
+        
+        // https://leetcode.com/explore/featured/card/top-interview-questions-easy/127/strings/884/discuss/4643/Java-Solution-with-4-steps-explanations
+        int index = 0;
+        int total = 0;
+        int sign = 1;
+        int n = s.length();
+        
+        // Check if empty string
+        if(s.length() == 0)
+            return 0;
+        
+        // remove white spaces from the string
+        while(index < n && s.charAt(index) == ' ')
+            index++;
+        
+        // Edge Case of returning 0;
+        if(index == n)
+            return 0;
+        
+        // get the sign
+        if(s.charAt(index) == '+')
+        {
+            index++;
+            sign = 1;
+        }
+        else if(s.charAt(index) == '-')
+        {
+            index++;
+            sign = -1;
+        }
+        
+        // convert to the actual number and make sure it's not overflow
+        while(index < n)
+        {
+            int digit = s.charAt(index) - '0';
+            
+            if(digit < 0 || digit > 9) 
+                break;
+            
+            // check for overflow
+            if(Integer.MAX_VALUE / 10 < total || Integer.MAX_VALUE / 10 == total && 
+               Integer.MAX_VALUE % 10 < digit)
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            
+			    // if (sign == 1 && digit > Integer.MAX_VALUE)
+			    // return Integer.MAX_VALUE;
+			    // if (sign == -1 && (-1) * digit < Integer.MIN_VALUE)
+			    // return Integer.MIN_VALUE;
+            
+            total = total*10 + digit;
+            
+            index++; // don't forget to increment the counter
+        }
+        
+        return total * sign;
+        
+    }
+    
+    public static int HowNotToSolve(String s)
+    {
+        //         String temp = "";
+        
+        //         boolean isNegative = false;
+        //         boolean isFirstZero = false;
+
+        //         for(int i = 0; i < s.length(); i++)
+        //         {
+        //             while(s.charAt(i) == ' ')
+        //                 i++;
+
+        //             if(i < s.length())
+        //             {
+        //                 if(s.charAt(i) == '0' && isFirstZero == false)
+        //                 {
+        //                     isFirstZero = true;
+        //                 }                  
+        //                 else if(s.charAt(i) == '-')
+        //                     isNegative = true;
+        //                 else if(Character.isDigit(s.charAt(i)))
+        //                 {
+        //                     temp += s.charAt(i);
+        //                 }
+        //             }
+        //         }
+
+        //         if(temp.length() != 0)
+        //         {
+        //             //double digit = Double.parseDouble(temp);
+
+        //             int sum = sum * 10 + str.charAt(i) - '0';
+        //             if (sign == 1 && sum > Integer.MAX_VALUE)
+        //                 return Integer.MAX_VALUE;
+        //             if (sign == -1 && (-1) * sum < Integer.MIN_VALUE)
+        //                 return Integer.MIN_VALUE;
+        //             if(digit >= 2*31 - 1)
+        //                 return 2*31 - 1;
+        //             else if(digit <= -2*31)
+        //                 return -2*31;
+
+        //             if(isNegative == true)
+        //                 digit = -digit;
+
+        //             return (int)digit;
+        //         }
+
+                 return 0;
+    }
+}
+
+    
+    
