febd62a9e163b63479d0fb45f5ced4895dd6e091
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-07-29 00:08:05 +0530

    Time: 0 ms, Memory: 40.2 MB - LeetHub

---
+++ implement-strstr/implement-strstr.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+        // empty needle appears everywhere, first appears at 0 index
+        if (needle.length() == 0)
+            return 0;
+        if (haystack.length() == 0)
+            return -1;
+        
+        int n = haystack.length();
+        int m = needle.length();
+        
+        String A = haystack;
+        String B = needle;
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(i + needle.length() > haystack.length())
+                break;
+                
+            for(int j = 0; j < m; j++)
+            {
+                if(B.charAt(j) != A.charAt(i+j))
+                    break;
+                
+                if(j == m - 1)
+                    return i;
+            }
+        }
+        
+        return -1;     
+    }
+}
