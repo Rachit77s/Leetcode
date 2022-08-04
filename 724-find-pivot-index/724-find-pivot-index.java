7103b338743ef2cc7cc8d15d839b1c45ba652319
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-08-05 01:02:48 +0530

    Time: 3 ms (36.22%), Space: 53.6 MB (21.32%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int[] prefixSum = new int[nums.length];
+        
+        prefixSum[0] = nums[0];
+        for(int i=1; i < nums.length; i++)
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+        
+        for(int i = 0; i < nums.length; i++)
+        {
+            int leftSum = 0;
+            int rightSum = 0;
+            if(i <= 0)
+                leftSum = 0;
+            else
+                leftSum = prefixSum[i-1];
+            
+            if(rightSum >= nums.length)
+                rightSum = 0;
+            else
+                rightSum = prefixSum[nums.length-1] - prefixSum[i];
+            
+            if(leftSum == rightSum)
+                return i;
+        }
+        
+        return -1;
+    }
+}
+
+// [1,7,3,6,5,6] = 28
+//.      17      28-17
