651eb01f0061b4f16f3ac64b309dfda13aa01fae
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-08-12 19:43:38 +0530

    Time: 8 ms, Memory: 96.6 MB - LeetHub

---
+++ palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,95 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        return Helper(head);
+    }
+    
+    public boolean Helper(ListNode head) 
+    {
+        // Love Babbar: https://www.youtube.com/watch?v=aD7mBVnKFEU
+        
+        /*
+            Steps:
+            1. Find the middle element of the LL
+            2. Reverse the list 
+            3. Compare both the halves of the LL
+            4: Make the LL back to original LL i.e. reverse 2nd half again(OPTIONAL STEP)
+        */
+        
+        if(head == null || head.next == null)
+            return true;
+        
+        // 1. Find the middle element
+        ListNode middle = GetMiddle(head);
+        
+        // 2. Reverse the list after the middle element
+        ListNode temp = middle.next;
+        middle.next = Reverse(temp);
+        
+        // 3. Compare both the halves of the LL
+        ListNode head1 = head;
+        ListNode head2 = middle.next;
+        
+        while(head2 != null)
+        {
+            if(head2.val != head1.val)
+                return false;
+            
+            head2 = head2.next;
+            head1 = head1.next;
+        }
+        
+        
+        // 4: Make the LL back to original LL i.e. reverse 2nd half again(OPTIONAL STEP)
+        temp = middle.next;
+        middle.next = Reverse(temp);
+        
+        return true;
+    }
+    
+    public ListNode GetMiddle(ListNode head) 
+    {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+    
+    public ListNode Reverse(ListNode head) 
+    {
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = null;
+        
+        while(curr != null)
+        {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+    }
+}
+
+// 1 --> 2 --> 2 --> 1
+// 1 --> 2 --> 3 --> 2 --> 1
+// 1 --> 2 --> 2 --> 3 --> 2 --> 2 --> 1
+
+// 1. Put in Array 1 2 2 1
