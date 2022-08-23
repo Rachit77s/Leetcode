3139ac0dafd1440a973c07d8920a8c0478e0e251
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-08-23 10:42:28 +0530

    Time: 2 ms (34.94%), Space: 46 MB (6.39%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -16,7 +16,47 @@
 class Solution {
     public boolean isBalanced(TreeNode root) {
         
-        return IsHeightBalancedBruteForce(root);
+        return IsHeightBalancedOptimised(root).isBalanced;
+        
+        //return IsHeightBalancedBruteForce(root);
+    }
+    
+    public class Info 
+    {
+        public boolean isBalanced;
+        public int height;
+
+        public Info (boolean isBalanced, int height)
+        {
+            this.isBalanced = isBalanced;
+            this.height = height;
+        }
+    }
+
+    public Info IsHeightBalancedOptimised(TreeNode root)
+    {
+        /*
+            What is the minimum information every node requires from its subtrees
+            1. Height of its LST.
+            2. Height of its RST.
+            3. Whether the LST and RST are balanced or not.
+        */
+        
+        if(root == null)
+            return new Info(true, 0);
+
+        // Check for each and every node
+        Info left = IsHeightBalancedOptimised(root.left);
+        Info right = IsHeightBalancedOptimised(root.right);
+        // Access properties of left and right and check the condition
+        boolean condition = Math.abs(left.height - right.height) <= 1;
+        
+        // If all the 3 conditions match, return true.
+        if(left.isBalanced == true && right.isBalanced == true && condition == true)
+            return new Info(true, 1 + Math.max(left.height, right.height)); 
+        // Return the height as well.
+        else
+            return new Info(false, 0);
     }
     
     public boolean IsHeightBalancedBruteForce(TreeNode root)
