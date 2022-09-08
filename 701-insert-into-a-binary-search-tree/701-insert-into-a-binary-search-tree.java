5a338346be0801bb827ef2ee3a73bd1dc9df4c66
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-09-09 00:03:31 +0530

    Time: 0 ms (100.00%), Space: 54.2 MB (57.17%) - LeetHub

---
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
@@ -0,0 +1,115 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        
+        if(root == null)
+            return new TreeNode(val);
+        
+        Recursion(root, val);
+        return root;
+    }
+    
+    public TreeNode Recursion(TreeNode root, int K) 
+    {
+        /*
+        Assumption:This fn inserts K at appropriate position in the tree
+        rooted at 'node' and returns the updated root node.
+        */
+        
+        if(root == null)
+            return new TreeNode(K);
+        
+        if(root.val > K)
+            root.left = Recursion(root.left, K);
+        else
+            root.right =  Recursion(root.right, K);
+        
+        return root;
+    }
+    
+    public TreeNode Iterative1(TreeNode root, int K) 
+    {   
+        if(root == null)
+            return new TreeNode(K);
+        
+        TreeNode curr = root;
+        
+        while(curr != null)
+        {
+            // Insert in LST
+            if(curr.val > K)
+            {
+                // Go left till curr is not null
+                if(curr.left != null)
+                    curr = curr.left;
+                else
+                {
+                    curr.left = new TreeNode(K);
+                    break;
+                }
+            }
+            else
+            {
+                // Go right till curr is not null
+                if(curr.right != null)
+                    curr = curr.right;
+                else
+                {
+                    curr.right = new TreeNode(K);
+                    break;
+                }
+            }
+        }
+        
+        return root;
+    }
+    
+    public TreeNode Iterative2(TreeNode root, int K) 
+    {
+        if(root == null)
+            return new TreeNode(K);
+        
+        TreeNode curr = root;
+        // Use parent pointer 
+        TreeNode parent = null;
+        
+        while(curr != null)
+        {         
+            parent = curr;
+            if(curr.val > K)
+            {
+                curr = curr.left;
+            }
+            else
+            {
+                curr = curr.right;
+            }
+        }
+        
+        // Check where should new value be added wrt its parent
+        if(parent.val > K)
+        {
+            parent.left = new TreeNode(K);
+        }
+        else
+        {
+            parent.right = new TreeNode(K);
+        }
+        
+        return root;
+    }
+}
