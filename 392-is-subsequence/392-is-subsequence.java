e10120a795c06c80ee3ff59c56c87abcc9298eff
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-09-25 13:52:39 +0530

    Time: 6 ms (11.72%), Space: 41.5 MB (80.76%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+        int j = 0;
+        int i = 0;
+        int cnt = 0;
+        
+        if (s.length() > t.length()) 
+            return false;
+        
+        if (s.length() == 0) 
+            return true;
+        
+        while(i < s.length() && j < t.length())
+        {
+            if(s.charAt(i) == t.charAt(j))
+            {
+                i++;
+                cnt++;
+            }
+                
+            
+            j++;    
+        }
+        
+        // if(i != t.length())
+        //     return false;
+        
+        if(cnt != s.length())
+            return false;
+        
+        return true;
+    }
+}
+
+// "ahbgdc"
+// abc
