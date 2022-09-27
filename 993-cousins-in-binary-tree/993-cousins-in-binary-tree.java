d6a67391de1df909962d72cc13cc87301297f132
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-09-27 16:13:29 +0530

    Time: 1 ms (64.78%), Space: 41.9 MB (47.60%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
@@ -26,15 +26,11 @@ class Solution {
           To be a cousins, below condition should met
             1. Level should be same
             2. Parent node should be different i.e. parent should not be same for 2 nodes
-        */
-        
-        /*
+
             Aliter Sol 1: Check level of two nodes x and y(it should be equal), and
                    if curr left and right is x and y, 
                    it means they have same parent, return false
-        */
-        
-        /*
+
             Algo:
             1. Assume x & y nodes are not there, so bool false.
             2. For every level, check if curr node is x & y and also
@@ -50,6 +46,8 @@ class Solution {
         
         while(!q.isEmpty())
         {
+            // To check x & y present at the same level or not
+            // If they are at same level, both x & y will be true
             boolean xExists = false;
             boolean yExists = false;
             int qSize = q.size();
@@ -58,13 +56,15 @@ class Solution {
             {
                 TreeNode curr = q.poll();
                 
+                // Check if x is present
                 if(curr.val == x)
                     xExists = true;
                 
+                // Check if y is present
                 if(curr.val == y)
                     yExists = true;
                 
-                // Check if the curr node is parent of its children, return false
+                // Check if curr node is parent of x & y, if yes return false
                 if(curr.left != null && curr.right != null)
                 {
                     if(curr.left.val == x && curr.right.val == y)
@@ -81,7 +81,7 @@ class Solution {
                     q.offer(curr.right);
             }
             
-            // Found diff nodes whose left and right child is equal to x&y
+            // Found two diff nodes whose left and right child is equal to x & y
             if(xExists == true && yExists == true)
                 return true;
         }
