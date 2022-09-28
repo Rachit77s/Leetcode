5581c3ed893fda3ba208f8f1e72999f9f78e7943
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-09-28 16:05:48 +0530

    Time: 2 ms (85.16%), Space: 44.3 MB (72.33%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.java
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -18,7 +18,8 @@ class Solution {
         
         List<List<Integer>> ans = new ArrayList<>();
         
-        Backtrack(root, targetSum, ans, new ArrayList<Integer>());
+        BacktrackV2(root, targetSum, ans, new ArrayList<Integer>());
+        // Backtrack(root, targetSum, ans, new ArrayList<Integer>());
         
         return ans;
     }
@@ -41,6 +42,29 @@ class Solution {
         Backtrack(root.left, targetSum - root.val, ans, tempList);
         Backtrack(root.right, targetSum - root.val, ans, tempList);
         
+        tempList.remove(tempList.size() - 1);  
+    }
+    
+    public void BacktrackV2(TreeNode root, int targetSum, List<List<Integer>> ans,
+                                      List<Integer> tempList) 
+    {
+        if(root == null)
+            return;
+        
+        tempList.add(root.val);
+        
+        if(root.left == null && root.right == null && targetSum == root.val)
+        {
+            ans.add(new ArrayList<Integer>(tempList));
+             // if (targetSum == 0) // Found valid path
+             //    ans.push_back(path);
+        }
+        else
+        {
+            BacktrackV2(root.left, targetSum - root.val, ans, tempList);
+            BacktrackV2(root.right, targetSum - root.val, ans, tempList);
+        }
+
         tempList.remove(tempList.size() - 1);  
     }
 }
