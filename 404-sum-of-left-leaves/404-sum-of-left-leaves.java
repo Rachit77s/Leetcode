f59f2ee56da72f9c97f085f9dbebcfc0e4bb397f
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-09-29 14:25:58 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (23.27%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.java
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.java
@@ -16,7 +16,9 @@
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
         
-        return Level(root);
+        return Recursive(root);
+        
+        // return Level(root);
     }
     
     public int Level(TreeNode root)
@@ -54,6 +56,56 @@ class Solution {
         }
 
         return sum;
+    }
+    
+    public int StackDFS(TreeNode root)
+    {   
+        if(root == null) 
+            return 0;
+
+        // Single node
+        if(root.left == null && root.right == null)
+            return 0;
+        
+        int sum = 0;
+        Stack<TreeNode> stk = new Stack<TreeNode>();
+        TreeNode curr = root;
+
+        while(!stk.isEmpty() || curr != null)
+        {
+            if(curr != null)
+            {
+                if(curr.left != null && curr.left.left == null && curr.left.right == null)
+                    sum = sum + curr.left.val;
+                
+                stk.push(curr);
+                curr = curr.left;
+            }
+            else
+            {
+                TreeNode temp = stk.pop();
+                
+                if(temp.right != null && temp.right.left == null)
+                    sum = sum + temp.right.val;
+                
+                curr = temp.right;
+            }
+        }
+
+        return sum;
+    }
+    
+    public int Recursive(TreeNode root)
+    {
+        if(root == null) 
+            return 0;
+        
+        int result = 0;
+        
+        if(root.left != null && root.left.left == null && root.left.right == null)
+                result = result + root.left.val;
+        
+        return result + Recursive(root.left) + Recursive(root.right);
     }
 }
 
