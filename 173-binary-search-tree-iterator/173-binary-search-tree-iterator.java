d6b633d30dab889974643c82c7948dbacf223c72
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-10-02 18:19:03 +0530

    Time: 28 ms (58.82%), Space: 51.2 MB (81.42%) - LeetHub

---
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// Striver: https://www.youtube.com/watch?v=D2jMcmxU4bs
+class BSTIterator {
+
+    Stack<TreeNode> stk = new Stack<>();
+    
+    public BSTIterator(TreeNode root) {
+        // Go all the way on the left while adding in stk till we encounter null    
+        pushAll(root);
+    }
+    
+    public int next() {
+        // Pop the top most element, go to right and push all the left nodes in stack.
+        TreeNode temp = stk.pop();
+        pushAll(temp.right);
+        return temp.val;
+    }
+    
+    // If stk is not empty return it is not null
+    public boolean hasNext() {
+        return !stk.isEmpty();
+    }
+    
+    //My function
+    // Take all the left's and push in the stack
+    private void pushAll(TreeNode root)
+    {
+        while(root != null)
+        {
+            stk.push(root);
+            root = root.left;
+        }
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
