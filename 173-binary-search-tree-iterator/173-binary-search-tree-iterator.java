58a4c0ff69495f043ebc1ff5c121b502dbb7f273
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-10-03 15:09:40 +0530

    Time: 15 ms (97.01%), Space: 46 MB (97.09%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
@@ -17,69 +17,69 @@
 // Striver: https://www.youtube.com/watch?v=D2jMcmxU4bs
 class BSTIterator {
 
-    Stack<TreeNode> stack = new Stack<>();
+    Stack<TreeNode> stk = new Stack<>();
     
-//     public BSTIterator(TreeNode root) {
-//         // Go all the way on the left while adding in stk till we encounter null    
-//         pushAll(root);
-//     }
-    
-//     public int next() {
-//         // Pop the top most element, go to right and push all the left nodes in stack.
-//         TreeNode temp = stk.pop();
-//         pushAll(temp.right);
-//         return temp.val;
-//     }
-    
-//     // If stk is not empty I have a list, if it is empty, I don't have list
-//     public boolean hasNext() {
-//         return !stk.isEmpty();
-//     }
-    
-//     //My function
-//     // Take all the left's and push in the stack
-//     private void pushAll(TreeNode root)
-//     {
-//         while(root != null)
-//         {
-//             stk.push(root);
-//             root = root.left;
-//         }
-//     }
-    
-    public BSTIterator(TreeNode root)
-    {
-        stack = new Stack<>();
-        //this.isForward = flag;
-        
-        // Go to extreme left while inserting in stack
+    public BSTIterator(TreeNode root) {
+        // Go all the way on the left while adding in stk till we encounter null    
         pushAll(root);
     }
     
-    public boolean hasNext()
-    {
-        return !stack.isEmpty();
-    }
-    
-    public int next()
-    {
-        // Pop the top most element, go to its right and 
-        // push all the left nodes in stack.
-        TreeNode temp = stack.pop();
+    public int next() {
+        // Pop the top most element, go to right and push all the left nodes in stack.
+        TreeNode temp = stk.pop();
         pushAll(temp.right);
-        
         return temp.val;
     }
     
+    // If stk is not empty I have a list, if it is empty, I don't have list
+    public boolean hasNext() {
+        return !stk.isEmpty();
+    }
+    
+    //My function
     // Take all the left's and push in the stack
-    public void pushAll(TreeNode root)
+    private void pushAll(TreeNode root)
     {
         while(root != null)
         {
-            stack.push(root);
+            stk.push(root);
             root = root.left;
         }
     }
+    
+//     public BSTIterator(TreeNode root)
+//     {
+//         stack = new Stack<>();
+//         //this.isForward = flag;
+        
+//         // Go to extreme left while inserting in stack
+//         pushAll(root);
+//     }
+    
+//     public boolean hasNext()
+//     {
+//         return !stack.isEmpty();
+//     }
+    
+//     public int next()
+//     {
+//         // Pop the top most element, go to its right and 
+//         // push all the left nodes in stack.
+//         TreeNode temp = stack.pop();
+//         pushAll(temp.right);
+        
+//         return temp.val;
+//     }
+    
+//     // Take all the left's and push in the stack
+//     public void pushAll(TreeNode root)
+//     {
+//         while(root != null)
+//         {
+//             stack.push(root);
+//             root = root.left;
+//         }
+//     }
     
 }
 
