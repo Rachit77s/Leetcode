3a1f2df21a19604731db65077b5c723271ce0061
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-10-14 11:33:50 +0530

    Time: 241 ms (8.36%), Space: 108.9 MB (39.22%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -18,8 +18,8 @@ class Solution {
     
     public int pseudoPalindromicPaths (TreeNode root) {
         
-   
-        return RecursiveHelper(root);
+        return HelperUsingSet(root);
+        // return RecursiveHelper(root);
     }
     
     public int RecursiveHelper(TreeNode root)
@@ -74,6 +74,43 @@ class Solution {
         }
         
         return true;
+    }
+    
+    public int HelperUsingSet(TreeNode root)
+    {
+        HashSet<Integer> set = new HashSet<>();
+        RecursiveUsingSet(root, set);
+        return ans;
+    }
+    
+    // Time O(N)
+    // Space O(K + H)
+    // Comments: https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/discuss/648345/Java-DFS-%2B-Set-%2B-Recursion
+    // https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/discuss/648534/JavaC%2B%2BPython-At-most-one-odd-occurrence
+    public void RecursiveUsingSet(TreeNode root, HashSet<Integer> set)
+    {
+    // INTUITION - Palindrome can have all duplicates or can have at max 1 unique element.
+    // Whenever u find a duplicate in a path, just remove it.
+    // At the leaf node, either there will be no element, or there can be at max 1 number.
+        
+        if(root == null)
+            return;
+        
+        // duplicate found, so remove it
+        if(set.contains(root.val))
+            set.remove(root.val);
+        else
+            set.add(root.val);
+        
+        if(root.left == null && root.right == null)
+        {
+            if(set.size() <= 1)
+                ans++;
+        }
+        
+        // Because if we dont pass a copy of the HashSet, then the HashSet will get modified by its subtree. Meaning - the HashSet will be altered by the left subtree according to its needs and then the right subtree will not get intended HashSet. Thats why we need to pass on a copy of the HashSet (at current level) to its next level.
+        RecursiveUsingSet(root.left, new HashSet<>(set));
+        RecursiveUsingSet(root.right, new HashSet<>(set));
     }
 }
 
