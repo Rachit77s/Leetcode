8a433bdfe58a345a7a809e371d62be5276ae74d1
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-10-15 17:42:15 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (85.63%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -0,0 +1,79 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+        //return LevelOrder(root);
+        Invert2(root);
+        return root;
+    }
+    
+    public void Invert(TreeNode root) 
+    {
+        if(root == null)
+            return;
+        
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        Invert(root.left);
+        Invert(root.right);
+    }
+    
+    public void Invert2(TreeNode root) 
+    {
+        if(root == null)
+            return;
+        
+        Invert2(root.left);
+        Invert2(root.right);
+        
+                
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+    }
+    
+    public TreeNode LevelOrder(TreeNode root)
+    {
+        if(root == null)
+            return null;
+        
+        Queue<TreeNode> q = new LinkedList();
+        q.add(root);
+        
+        while (q.size() > 0)
+        {
+            // pop top node from queue
+            TreeNode curr = q.peek();
+            q.remove();
+
+            // swap left child with right child
+            TreeNode temp = curr.left;
+            curr.left = curr.right;
+            curr.right = temp;
+
+            // push left and right children
+            if (curr.left != null)
+                q.add(curr.left);
+            if (curr.right != null)
+                q.add(curr.right);
+        }
+        
+        return root;
+    }
+}
