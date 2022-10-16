e9eaf1bc42be51ce842190d009238adb4b77d782
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-10-16 09:44:40 +0530

    Time: 18 ms (13.64%), Space: 42.4 MB (50.44%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,6 +1,8 @@
 class Solution {
     public int longestPalindrome(String s) {
-        return Helper(s);
+        
+        return Optimized(s);
+        // return Helper(s);
     }
     
     public int Helper(String s) 
@@ -20,6 +22,9 @@ class Solution {
                 map.put(s.charAt(i), 1);
         }
         
+        // We need to count odd occurrences
+        // For 1st time, if odd comes, increase the count as it is.
+        // From 2nd time onwards, if odd comes do count - 1 as we have considered 1 odd sequence
         for (Map.Entry<Character, Integer> entry : map.entrySet()) 
         {
             Character key = entry.getKey();
@@ -29,9 +34,7 @@ class Solution {
             {
                 // encountering 2nd time
                 if(flag == true)
-                {
                     ans = ans + value - 1;
-                }
                 else
                 {
                     ans = ans + value;
@@ -39,10 +42,37 @@ class Solution {
                 }
             }
             else
+            {
                 ans = ans + value;
+            }      
         }
         
         return ans;
+    }
+    
+    // Comments: https://leetcode.com/problems/longest-palindrome/discuss/89604/Simple-HashSet-solution-Java
+    public int Optimized(String s) 
+    {
+        int count = 0;
+        HashSet<Character> set = new HashSet<>();
+        
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(set.contains(s.charAt(i)))
+            {
+                // All even no chars will be removed at the end
+                set.remove(s.charAt(i));
+                count++;
+            }
+            else
+                set.add(s.charAt(i));
+        }
+        
+        // Only odd character will remain in the set at the end
+        if(!set.isEmpty())
+            return count * 2 + 1; // include that 1 odd sequence
+
+        return count * 2; // All even chars were there in the string
     }
 }
 
