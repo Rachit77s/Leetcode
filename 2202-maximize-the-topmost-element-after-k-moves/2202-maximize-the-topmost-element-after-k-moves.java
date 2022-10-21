b6076869fd560f32faf71bd05372e88d36d71c9f
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-10-21 22:54:39 +0530

    Time: 2 ms (67.62%), Space: 84.5 MB (21.31%) - LeetHub

---
+++ 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maximumTop(int[] nums, int k) {
+         if (nums.length == 1 && k % 2 == 1) return -1; // if size is 1 and k odd stack will be empty
+        int max = 0;
+        for (int i = 0; i < Math.min(k - 1 ,nums.length); i++) //finding the max element from first k-1 elelment or len -1 if len is less than k
+            max = Math.max(max, nums[i]);
+        if (k < nums.length)  // check for scenario where we dont have to put back Max out of k-1 element
+            max = Math.max(max, nums[k]);
+        return max;
+    }
+}
