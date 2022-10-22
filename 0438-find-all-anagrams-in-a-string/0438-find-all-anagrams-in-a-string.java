1c189e7d184ae511d2391966e1787160ec5bf4f4
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-10-23 00:27:01 +0530

    Time: 13 ms (82.56%), Space: 47.6 MB (68.36%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,7 +1,61 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         
-        return Helper(s, p);
+        return AnagramHelper(s, p);
+    }
+    
+    public List<Integer> AnagramHelper(String largerString, String smallerAnagramString)
+    {
+        List<Integer> ans = new ArrayList<Integer>();
+        
+        if(smallerAnagramString.length() > largerString.length())
+            return ans;
+
+        int[] hashOfLarger = new int[26];
+        int[] hashOfSmaller = new int[26];
+
+        for (int i = 0; i < smallerAnagramString.length(); i++)
+        {
+            hashOfLarger[largerString.charAt(i)- 'a']++;
+            hashOfSmaller[smallerAnagramString.charAt(i) - 'a']++;
+        }
+
+        // Initial Comparison: Compare the frequency of each char of the array
+        if(CompareTwoHashArrays(hashOfLarger, hashOfSmaller))
+        {
+            ans.add(0);
+        }
+
+        // We have already compared till initial length, hence start from A.Length
+        int left = 0;
+        int right = smallerAnagramString.length();
+        while(right < largerString.length())
+        {
+            // Increase the sliding window
+            hashOfLarger[largerString.charAt(right) - 'a']++;
+            right++;
+
+            // Decrease the older element count i.e. window of Length of anagramString
+            hashOfLarger[largerString.charAt(left) - 'a']--;
+            left++;
+
+            // Further Comparison: Compare the frequency of each element of the array
+            if(CompareTwoHashArrays(hashOfLarger, hashOfSmaller))
+                ans.add(left);
+        }
+
+        return ans;
+    }
+
+    public boolean CompareTwoHashArrays(int[] arrOfA, int[] arrOfB)
+    {
+        for(int i = 0; i < 26; i++)
+        {
+            if(arrOfA[i] != arrOfB[i])
+                return false;
+        }
+
+        return true;
     }
     
     public List<Integer> Helper(String s, String p)
