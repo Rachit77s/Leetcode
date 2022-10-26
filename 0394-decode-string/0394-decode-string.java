c4c8237683078602be3bfe4ba65a12ab9cc93ab5
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-10-26 18:25:51 +0530

    Time: 1 ms (93.71%), Space: 42.3 MB (31.61%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,9 +1,54 @@
 class Solution {
     public String decodeString(String s) {
         
-        return Helper(s);
+        return Helper1(s);
     }
     
+    // Good One: https://www.youtube.com/watch?v=SF2W6VDs7bc
+    public String Helper1(String s)
+    {
+        // Keep track of counts
+        Stack<Integer> countStk = new Stack<>();
+        Stack<String> wordStk = new Stack<>();
+        StringBuilder word = new StringBuilder();
+        int number = 0;
+        
+        for(int i = 0; i < s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            
+            // 300[a2[c]]
+            
+            if(ch >= '0' && ch <= '9')
+            {
+                number = number * 10 + ch - '0';
+            }
+            else if(ch == '[')
+            {
+                countStk.push(number);
+                wordStk.push(word.toString());
+                
+                number = 0;
+                word = new StringBuilder();
+            }
+            else if(ch == ']')
+            {
+                int cnt = countStk.pop();
+                StringBuilder duplicatedWord = new StringBuilder(wordStk.pop());
+                
+                for(int j = 0; j < cnt; j++)
+                    duplicatedWord.append(word);
+                
+                word = duplicatedWord;
+            }
+            else
+                word.append(ch);
+        }
+        
+        return word.toString();
+    }
+    
+    // https://www.youtube.com/watch?v=0iQqj5egK9k
     public String Helper(String s)
     {
         // Keep track of counts
@@ -30,6 +75,7 @@ class Solution {
                     
                 countsStk.push(count);
             }
+            // Append at opening bracket
             else if(ch == '[')
             {
                 resultStk.push(ans);
