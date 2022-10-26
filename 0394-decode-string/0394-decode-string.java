f306884778cca5ecb6d58fd629de985ac9d0eae1
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-10-26 17:12:38 +0530

    Time: 10 ms (14.59%), Space: 42.3 MB (31.61%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,36 +1,61 @@
 class Solution {
     public String decodeString(String s) {
-        String res = "";
-        Stack<Integer> countStack = new Stack<>();
-        Stack<String> resStack = new Stack<>();
-        int idx = 0;
-        while (idx < s.length()) {
-            if (Character.isDigit(s.charAt(idx))) {
-                int count = 0;
-                while (Character.isDigit(s.charAt(idx))) {
-                    count = 10 * count + (s.charAt(idx) - '0');
-                    idx++;
-                }
-                countStack.push(count);
-            }
-            else if (s.charAt(idx) == '[') {
-                resStack.push(res);
-                res = "";
-                idx++;
-            }
-            else if (s.charAt(idx) == ']') {
-                StringBuilder temp = new StringBuilder (resStack.pop());
-                int repeatTimes = countStack.pop();
-                for (int i = 0; i < repeatTimes; i++) {
-                    temp.append(res);
-                }
-                res = temp.toString();
-                idx++;
-            }
-            else {
-                res += s.charAt(idx++);
-            }
-        }
-        return res;
+        
+        return Helper(s);
     }
+    
+    public String Helper(String s)
+    {
+        // Keep track of counts
+        Stack<Integer> countsStk = new Stack<>();
+        Stack<String> resultStk = new Stack<>();
+        
+        String ans = "";
+        int index = 0;
+        
+        while(index < s.length())
+        {
+            char ch = s.charAt(index);
+            
+            if(ch >= '0' && ch <= '9')
+            {
+                // Number can be large like 3650[ab]
+                int count = 0;
+                
+                while(Character.isDigit(s.charAt(index)))
+                {
+                    count = count * 10 + s.charAt(index) - '0';
+                    index += 1;
+                }
+                    
+                countsStk.push(count);
+            }
+            else if(ch == '[')
+            {
+                resultStk.push(ans);
+                ans = "";
+                index += 1;
+            }
+            else if(ch == ']')
+            {
+                StringBuilder sb = new StringBuilder(resultStk.pop());
+                int cnt = countsStk.pop();
+                
+                for(int i = 0; i < cnt; i++)
+                    sb.append(ans);
+                
+                ans = sb.toString();
+                index += 1;
+            }
+            else
+            {
+                // Add onto the string 
+                ans += ch;
+                index += 1;
+            }   
+        }
+        
+        return ans;
+    }
+    
 }
