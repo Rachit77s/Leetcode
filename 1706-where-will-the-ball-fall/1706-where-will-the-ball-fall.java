ebb73f3d6e54c951c72094eab299b7147f145b54
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-11-01 19:38:33 +0530

    Time: 20 ms (10.94%), Space: 54.6 MB (24.90%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -1,11 +1,55 @@
 class Solution {
     public int[] findBall(int[][] grid) {
         
-        return Helper(grid);
+        return Helper1(grid);
+    }
+    
+    // https://www.youtube.com/watch?v=Kyg-Rs37dTE
+    // Comments: https://leetcode.com/problems/where-will-the-ball-fall/discuss/988576/JavaC%2B%2BPython-Solution-with-Explanation
+    public int[] Helper1(int[][] grid)
+    {
+        if (grid == null || grid.length == 0) 
+            return new int[0];
+        
+        int[] ans = new int[grid[0].length];
+        
+        // Each loop computes the result for when be drop a ball in column i.
+        for (int i = 0; i < grid[0].length; ++i) 
+        {
+            int currRow = 0;
+            int currCol = i;
+            
+            while(currRow < grid.length)
+            {
+                // Move to right
+                // We go to the right if the current value and the value to the right are both equal to 1.
+                if(grid[currRow][currCol] == 1 && currCol + 1 < grid[0].length && 
+                   grid[currRow][currCol + 1] == 1)
+                {
+                    currRow++;
+                    currCol++;
+                }
+                // We go to the left if the current value and the value to the left are both equal to -1.
+                else if(grid[currRow][currCol] == -1 && currCol - 1 >= 0 && 
+                   grid[currRow][currCol - 1] == -1)
+                {
+                    currRow++;
+                    currCol--;
+                }
+                // If we can't move to the left, and we can't move to the right, then the ball is stuck because there is no other way to move.
+                else
+                    break;
+            }
+            
+            // Store -1 if the ball got stuck.
+            ans[i] = currRow == grid.length ? currCol : -1;   
+        }
+        
+        return ans;
     }
     
     // Pepcoding: https://www.youtube.com/watch?v=qCKXRsIItG8
-    public int[] Helper(int[][] grid) 
+    public int[] Helper2(int[][] grid) 
     {
         int row = grid.length;
         int col = grid[0].length;
