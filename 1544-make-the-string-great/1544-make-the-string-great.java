2b9201296b12d81f9bd485f7b4060ff69f02f38d
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-11-08 13:50:45 +0530

    Time: 5 ms (72.40%), Space: 43.1 MB (34.52%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String makeGood(String s) {
+        
+        return M1(s);
+    }
+    
+    public String M1(String s) 
+    {
+        Stack<Character> stack = new Stack();
+        
+        for(int i = 0; i < s.length(); i++)
+        {
+            // ASCII value of a is 97 and A is 65 , 97-65 = 32
+            if(!stack.isEmpty() && Math.abs(stack.peek() - s.charAt(i)) == 32)
+                stack.pop();
+            else
+                stack.push(s.charAt(i));
+        }
+        
+        char[] ans = new char[stack.size()];
+        int index = stack.size() - 1;
+        
+        while(!stack.isEmpty())
+        {
+            char ch = stack.pop();
+            ans[index] = ch;
+            index--;
+        }
+        
+        return new String(ans);
+    }
+}
