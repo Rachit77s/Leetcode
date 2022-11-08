33dc7b700a31b5a7cb513f8596fd959317fbea08
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-11-08 14:25:35 +0530

    Time: 6 ms (58.65%), Space: 42.5 MB (70.50%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,7 +1,8 @@
 class Solution {
     public String makeGood(String s) {
         
-        return M1(s);
+        return M2(s);
+        // return M1(s);
     }
     
     public String M1(String s) 
@@ -15,6 +16,32 @@ class Solution {
                 stack.pop();
             else
                 stack.push(s.charAt(i));
+        }
+        
+        char[] ans = new char[stack.size()];
+        int index = stack.size() - 1;
+        
+        while(!stack.isEmpty())
+        {
+            char ch = stack.pop();
+            ans[index] = ch;
+            index--;
+        }
+        
+        return new String(ans);
+    }
+    
+    public String M2(String s) 
+    {
+        Stack<Character> stack = new Stack();
+        
+        for(char c: s.toCharArray())
+        {
+            // ASCII value of a is 97 and A is 65 , 97-65 = 32
+            if(!stack.isEmpty() && ((stack.peek() - 'a' == c - 'A') || (stack.peek() - 'A' == c - 'a')))
+                stack.pop();
+            else
+                stack.push(c);
         }
         
         char[] ans = new char[stack.size()];
