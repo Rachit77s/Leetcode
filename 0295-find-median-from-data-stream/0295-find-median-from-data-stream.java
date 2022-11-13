ef1b968601a3102b295acc49a9ef6bdacf0777d5
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-11-13 16:11:29 +0530

    Time: 196 ms (72.62%), Space: 125.3 MB (36.83%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,26 +1,49 @@
 class MedianFinder {
 
-private PriorityQueue<Integer> small = new PriorityQueue<>(Collections.reverseOrder());
-private PriorityQueue<Integer> large = new PriorityQueue<>();
-private boolean even = true;
-
-public double findMedian() {
-    if (even)
-        return (small.peek() + large.peek()) / 2.0;
-    else
-        return small.peek();
-}
-
-public void addNum(int num) {
-    if (even) {
-        large.offer(num);
-        small.offer(large.poll());
-    } else {
-        small.offer(num);
-        large.offer(small.poll());
+    // By observation Numbers on the left side of Median are smaller than it, and the numbers on the right side of Median are greater than it. 
+// Median would be largest on the left side and smallest on the right side. Hence MaxHeap on the left side and MinHeap on the right side.
+    
+    // Code: Anuj: https://www.youtube.com/watch?v=Yv2jzDzYlp8
+    
+    // Assumption: MaxHeap can have atmost 1 more element than minHeap, 
+    // this is to find the ans for the odd length case.
+    private PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    
+    public MedianFinder() {
+        
     }
-    even = !even;
-}
+    
+    /*
+        1) store first half in maxh, second half in minh
+        2) at any point of time maxh can have atmost 1 element more than minh (==1 is the case when we have odd number of elements)
+    */
+    public void addNum(int num) 
+    {
+        // If number is less than maxHeap, add it to maxHeap
+        if(maxHeap.isEmpty() || maxHeap.peek() >= num)
+            maxHeap.add(num);
+        else
+            minHeap.add(num);
+        
+        // If maxHeap size is more than minHeap+1, pop element and add to minHp
+        if(maxHeap.size() > minHeap.size() + 1)
+            minHeap.add(maxHeap.poll());
+        else if(maxHeap.size() < minHeap.size())
+            maxHeap.add(minHeap.poll());
+    }
+    
+    public double findMedian() 
+    {
+        // Even case
+        if(maxHeap.size() == minHeap.size())
+        {
+            // Take the average of middle 2 elements of max and minHeap resp
+            return maxHeap.peek() / 2.0 + minHeap.peek() / 2.0;
+        }
+            
+        return maxHeap.peek();
+    }
 }
 
 /**
