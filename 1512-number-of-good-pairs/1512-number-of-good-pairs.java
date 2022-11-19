4a22a40c1b7aff5471aa490d31dda2ea5c13ca2f
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-11-19 22:40:54 +0530

    Time: 1 ms (89.67%), Space: 41.6 MB (44.70%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        
+        
+        return BruteForce(nums);
+    }
+    
+    public int BruteForce(int[] nums) 
+    {
+        int ans = 0;
+        
+        for (int i = 0; i < nums.length; i++) 
+        {
+            for (int j = i+1; j < nums.length; j++)
+            {
+                if(nums[i] == nums[j])
+                    ans++;
+            }
+        }
+        
+        return ans;
+    }
+}
