354da38e055bbf6ad28d4b95df9b8775bbaade33
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-11-21 18:25:34 +0530

    Time: 38 ms (11.29%), Space: 71.1 MB (55.72%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int nearestExit(char[][] maze, int[] entrance) {
+        Queue<int[]> bfs = new ArrayDeque<>();
+        bfs.offer(entrance);
+        
+        int rows = maze.length, cols = maze[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+        visited[entrance[0]][entrance[1]] = true;
+        
+        int steps = 1;
+        
+        while (!bfs.isEmpty()) {
+            int size = bfs.size();
+            for (int i = 1; i <= size; i++) {
+                int[] cell = bfs.poll();
+                int row = cell[0], col = cell[1];
+                
+                int[][] adjacentCells = new int[][]{{row+1, col}, {row, col+1}, {row-1, col}, {row, col-1}};
+                for (int[] adjacentCell : adjacentCells) {
+                    int adjacentRow = adjacentCell[0], adjacentCol = adjacentCell[1];
+                    
+                    if (adjacentRow < 0 || adjacentRow >= rows
+                        || adjacentCol < 0 || adjacentCol >= cols
+                        || maze[adjacentRow][adjacentCol] == '+')
+                        continue;
+                    
+                    if (!visited[adjacentRow][adjacentCol]) {       // important step checking for visited cell preventing loop
+                        if (adjacentRow == 0 || adjacentRow == rows - 1 
+                            || adjacentCol == 0 || adjacentCol == cols - 1)
+                            return steps;
+                        
+                        visited[adjacentRow][adjacentCol] = true;
+                        bfs.offer(new int[]{adjacentRow, adjacentCol});
+                    }
+                }
+            }
+            steps++;
+        }
+        
+        return -1;
+    }
+}
