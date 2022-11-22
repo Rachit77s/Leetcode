f7c9dbe1880d7a078624af5c06597b418ca6df1b
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-11-22 21:05:14 +0530

    Time: 30 ms (90.93%), Space: 43 MB (64.52%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,17 +1,43 @@
 class Solution {
     public int numSquares(int n) {
+        // int[] dp = new int[n + 1];
+        // Arrays.fill(dp, Integer.MAX_VALUE);
+        // dp[0] = 0;
+        // for(int i = 1; i <= n; ++i) {
+        //     int min = Integer.MAX_VALUE;
+        //     int j = 1;
+        //     while(i - j*j >= 0) {
+        //         min = Math.min(min, dp[i - j*j] + 1);
+        //         ++j;
+        //     }
+        //     dp[i] = min;
+        // }		
+        // return dp[n];
+        
+        return DPHelper(n);
+    }
+    
+    public int DPHelper(int n)
+    {
+        // Class question Satya Sai
         int[] dp = new int[n + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-        for(int i = 1; i <= n; ++i) {
+        
+        // Greedy approach won't work
+        
+        for(int i = 1; i <= n; i++)
+        {
             int min = Integer.MAX_VALUE;
-            int j = 1;
-            while(i - j*j >= 0) {
-                min = Math.min(min, dp[i - j*j] + 1);
-                ++j;
+            
+            for(int j = 1; j * j <= i; j++)
+            {
+                min = Math.min(min, 1 + dp[i - j * j]);
             }
+            
             dp[i] = min;
-        }		
+        }
+        
         return dp[n];
     }
 }
