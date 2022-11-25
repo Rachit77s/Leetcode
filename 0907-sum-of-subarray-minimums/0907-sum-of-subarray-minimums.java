7a308c851ec5c44668eabb552a379e486f8e0e2b
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-11-25 21:29:35 +0530

    Time: 186 ms (16.56%), Space: 71.9 MB (24.15%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,63 +1,54 @@
 class Solution {
+int mod=(int)Math.pow(10,9)+7;
+    public class Pair{
+        int e;
+        int c;
+        public Pair(int e, int c){
+            this.e = e;
+            this.c = c;
+        }
+    }
     public int sumSubarrayMins(int[] arr) {
         
-        return Comments(arr);
-        // return BruteForce(arr);
+        int n =arr.length;
+        int[] left = getLeftMin(arr, n);
+        int[] right = getRightMin(arr, n);
+        long sum=0;
+        for(int i=0; i<arr.length; i++){
+            sum = (sum+ (long)arr[i]*left[i]*right[i])%mod;
+        }
+        return (int)sum;
+        
     }
     
-    public int BruteForce(int[] A)
-    {
-        int sum = 0;
-        //int min = Integer.MAX_VALUE;
-        
-        for (int i = 0; i < A.length; i++) 
-        {
-            int min = A[i];
-            
-            for (int j = i; j < A.length; j++)
-            {
-                if(min > A[j])
-                    min = A[j];
-                
-                sum += min;
-                sum = sum % 1000000007;
-            }   
-        }
-        
-        return sum;     /// % 1000000007;
-    }
     
-    public int Comments(int[] arr) {
-      // initialize previous less element and next less element of 
-      // each element in the array
-        
-    int n = arr.length;
-
-        Stack<Integer> stack = new Stack<>();
-        // the distance of next or previous less
-        int[] left = new int[n];
-        int[] right = new int[n];
-
-        for(int i = 0; i < n; i++){
-            right[i] = n - i;
-        }
-
-        for(int i = 0; i < n; i ++){
-            while(!stack.isEmpty() && arr[stack.peek()] > arr[i]){
-                right[stack.peek()] = i - stack.peek();
-                stack.pop();
+    public int[] getLeftMin(int[] arr, int n) {
+        int[] a = new int[n];
+        Stack<Pair> s = new Stack<>();
+        for(int i = 0; i< n; i++){
+            int count = 1;
+            while(!s.isEmpty() && s.peek().e > arr[i]){
+                count+=s.peek().c;
+                s.pop();
             }
-            left[i] = stack.isEmpty() ? i+1 : i - stack.peek();
-            stack.push(i);
+            s.push(new Pair(arr[i], count));
+            a[i] = count;
         }
-
-        int res = 0;
-        int mod = (int)1e9 + 7;
-        for(int i = 0; i < n; i++){
-            res = (int) ((res + (long)arr[i]*left[i]*right[i])% mod);
+        return a;   
+    }
+    
+    public int[] getRightMin(int[] arr, int n) {
+        int[] a = new int[n];
+        Stack<Pair> s = new Stack<>();
+        for(int i = n-1; i>=0; i--){
+            int count = 1;
+            while(!s.isEmpty() && s.peek().e >= arr[i]){
+                count+=s.peek().c;
+                s.pop();
+            }
+            s.push(new Pair(arr[i], count));    
+            a[i] = count;  
         }
-        return res;
+        return a;
     }
 }
-
-
