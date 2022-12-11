0036318fbe8458539a53f47802f1d9ca291edc42
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-12-11 12:50:23 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (81.03%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,12 +1,33 @@
 class Solution {
+    
+    int[][] dp;
+    
     public int uniquePaths(int m, int n) {
-        int[] dp = new int[n]; 
-        dp[0] = 1; 
-        for (int i = 0; i < m; i++) { 
-            for (int j = 1; j < n; j++) { 
-                dp[j] += dp[j - 1]; 
-            } 
-        } 
-        return dp[n - 1]; 
+        
+        
+        return TopDownHelper(m, n);
     }
+    
+    // We can reach last cell from top cell or left cell
+    public int TopDownHelper(int m, int n)
+    {
+        dp = new int[m][n];
+        
+        return TopDownRecursion(m-1, n-1);
+    }
+    
+    public int TopDownRecursion(int i, int j)
+    {
+        if(i == 0 || j == 0)
+            return 1;
+        
+        if(dp[i][j] > 0)
+            return dp[i][j];
+        
+        dp[i][j] = TopDownRecursion(i-1, j) + TopDownRecursion(i, j-1);
+        
+        return dp[i][j];
+    }
+    
+    
 }
