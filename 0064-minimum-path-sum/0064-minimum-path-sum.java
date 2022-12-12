93b91ebd60af21c7751e3be157e7b30df75c8b44
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-12-12 14:31:35 +0530

    Time: 13 ms (29.95%), Space: 46.2 MB (30.14%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,7 +3,9 @@ class Solution {
     int[][]dp;
     public int minPathSum(int[][] grid) {
         
-        return TopDownHelper(grid);
+        return BottomUpOne(grid);
+        
+        // return TopDownHelper(grid);
     }
 
     public int TopDownHelper(int[][] A)
@@ -35,6 +37,65 @@ class Solution {
         else 
             return dp[i][j] = A[i][j] + 
                               Math.min(TopDown(A, i-1, j), TopDown(A, i, j-1));
+    }
+    
+    public int BottomUpOne(int[][] A)
+    {
+        int row = A.length;
+        int col = A[0].length;
+
+        dp = new int[row][col];
+
+        dp[0][0] = A[0][0];
+
+        for (int i = 1; i < row; i++)
+            dp[i][0] = A[i][0] + dp[i-1][0];
+
+        for (int j = 1; j < col; j++)
+            dp[0][j] = A[0][j] + dp[0][j - 1];
+
+
+        for (int i = 1; i < row; i++)
+        {
+            for (int j = 1; j < col; j++)
+            {
+                dp[i][j] = A[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return dp[row - 1][col - 1];
+    }
+
+    public int BottomUpTwo(int[][] A)
+    {
+        int row = A.length;
+        int col = A[0].length;
+
+        dp = new int[row][col];
+
+        dp[0][0] = A[0][0];
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (i == 0 && j > 0)
+                {
+                    dp[0][j] = A[0][j] + dp[0][j - 1];
+                }
+                else if(j == 0 && i > 0)
+                {
+                    dp[i][0] = A[i][0] + dp[i - 1][0];
+                }
+                else if(i > 0 && j > 0)
+                {
+                    dp[i][j] = A[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+                }
+                
+            }
+        }
+
+        return dp[row - 1][col - 1];
     }
     
 }
