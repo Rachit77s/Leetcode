ff7c44bf6bffdfb8fe2414bebb3e2d19f378f7a2
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-12-13 21:26:19 +0530

    Time: 2 ms (95.14%), Space: 47.1 MB (73.56%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,4 +1,7 @@
 class Solution {
+    
+    int[][] dp;
+    
     public int minFallingPathSum(int[][] matrix) {
         
         
@@ -28,7 +31,21 @@ class Solution {
         */
         
         
-        return Approach1(matrix);
+        // return Approach1(matrix);
+        return StriverApproachHelper(matrix);
+    }
+    
+    public void Notes()
+    {
+        /*
+            
+            dp[i]: min path sum to reach (i,j) from any cell in the 1st row.
+            
+            For last row each col figure out the min path sum.
+            F(n-1,0) F(n-1,1) F(n-1,2)
+            
+            Start from n-1 row and reach to 0th row
+        */
     }
     
     public int Approach1(int[][] A)
@@ -37,7 +54,7 @@ class Solution {
         int row = A.length;
         int col = A[0].length;
 
-        int[][] dp = new int[row][col];
+        dp = new int[row][col];
 
         int min = Integer.MAX_VALUE;
         for(int i = 0; i < A.length; i++)
@@ -71,5 +88,50 @@ class Solution {
             if(dp[A.length-1][i] < min)  min = dp[A.length-1][i];
         }
         return min;
+    }
+    
+    // Striver: https://www.youtube.com/watch?v=N_aJ5qQbYA0
+    public int StriverApproachHelper(int[][] A)
+    {
+        /*
+            7 Steps of DP
+            2.  What is the DP state? i.e. dp[i] ?
+
+            dp[i]: Min path sum to reach (i,j) last row each col from any cell
+            in the 1st row.
+        */
+        
+        int result = Integer.MAX_VALUE;
+        
+        int row = A.length;
+        int col = A[0].length;
+
+        dp = new int[row][col];
+
+        for(int[] currRow : dp) 
+            Arrays.fill(currRow, Integer.MAX_VALUE);
+        
+        for(int j = 0; j < col; j++)
+            result = Math.min(result, StriverApproachMemoization(A, row - 1, j));
+        
+        return result;
+    }
+    
+    public int StriverApproachMemoization(int[][] A, int i, int j)
+    {   
+        if(j < 0 || j >= A[i].length)
+            return Integer.MAX_VALUE;
+            
+        if(i == 0)
+            return dp[0][j] = A[0][j];
+        
+        if(dp[i][j] != Integer.MAX_VALUE) 
+            return dp[i][j];
+
+        int up = StriverApproachMemoization(A, i - 1, j);
+        int left = StriverApproachMemoization(A, i - 1, j - 1);
+        int right = StriverApproachMemoization(A, i - 1, j + 1);
+        
+        return dp[i][j] = Math.min(Math.min(left, right), up) + A[i][j];
     }
 }
