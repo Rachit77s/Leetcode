0638fd6b2cc6a2f2f94962c8166e792dbbb92aeb
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-12-14 22:23:22 +0530

    Time: 6 ms (85.03%), Space: 42.5 MB (82.56%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         
-        return DPApproach(s, wordDict);
+        return MemoizationApproach(s, wordDict);
         // return RecursiveHelper(s, wordDict);
         // return NeetcodeSolution(s, wordDict);
     }
@@ -81,4 +81,52 @@ class Solution {
         
         return dp[s.length()];
     }
+    
+public boolean MemoizationApproach(String s, List<String> wordDict) 
+{
+        // create the memoization array to save results and avoid repeat computations
+        Boolean[] dp = new Boolean[s.length()];
+
+        // convert the list into set for faster lookup
+        Set<String> wordSet = new HashSet<>(wordDict);
+
+        // recursion with memoization
+        return helper(s, 0, wordSet, dp);
+    }
+
+    private boolean helper(String s, int startIdx, Set<String> wordSet, Boolean[] dp) 
+    {
+        // in case we've reached the end of string, return true
+        if (startIdx == s.length()) 
+            return true;
+        
+        // else if we've already computed on current substring before
+        if (dp[startIdx] != null) 
+            return dp[startIdx]; // auto-unboxing
+
+        boolean res = false;
+        
+        // iterate through all indices after startIdx, explore every possible word
+        for (int i = startIdx + 1; i <= s.length(); i++) 
+        {
+            String currWord = s.substring(startIdx, i);
+            
+            // skip if this is not a word in the input dictionary
+            if (!wordSet.contains(currWord)) 
+                continue;
+
+            // recursively call upon the rest of string
+            if (helper(s, i, wordSet, dp)) 
+            {
+                res = true;
+                break;
+            }
+        }
+        
+        // add result to memo and return the result
+        dp[startIdx] = res;
+        
+        return res;
+    }
+
 }
