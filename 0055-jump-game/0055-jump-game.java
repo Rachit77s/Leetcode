437bd24c40ea6e0b7494376e62224bdde8592ae5
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-12-15 12:14:49 +0530

    Time: 2 ms (94.26%), Space: 43.1 MB (82.96%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,26 +1,43 @@
 class Solution {
     public boolean canJump(int[] nums) {
         
-        int n = nums.length;
+//         int n = nums.length;
         
-        int i = 0;
-        int currFarther = 0;
+//         int i = 0;
+//         int currFarther = 0;
         
-        if(nums[0] == 0 && n > 1)
-            return false;
+//         if(nums[0] == 0 && n > 1)
+//             return false;
         
-        while(i < n && i <= currFarther)
+//         while(i < n && i <= currFarther)
+//         {
+//             currFarther = Math.max(i+ nums[i], currFarther);
+//             // System.out.print(currFarther + " ");
+            
+//             if(i == currFarther)
+//                 break;
+            
+//             i++;       
+//         }
+        
+        
+//         return i >= n-1;
+        
+        return ApproachII(nums);
+    }
+    
+    public boolean ApproachII(int[] A)
+    {
+        int reach = 0;
+        
+        for(int i = 0; i <= reach; i++)
         {
-            currFarther = Math.max(i+ nums[i], currFarther);
-            // System.out.print(currFarther + " ");
+            reach = Math.max(reach, i + A[i]);
             
-            if(i == currFarther)
-                break;
-            
-            i++;       
+            if(reach >= A.length - 1)
+                return true;
         }
         
-        
-        return i >= n-1;
+        return false;
     }
 }
