b0cb0374dccf73281f2fa0c28b919497fb1a2d7e
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-12-28 13:31:47 +0530

    Time: 527 ms (88.04%), Space: 58.5 MB (70.00%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
@@ -1,9 +1,37 @@
 class Solution {
     public int minStoneSum(int[] piles, int k) {
         
-        return ApproachI(piles, k);
+        return BestApproach(piles, k);
+        // return ApproachI(piles, k);
     }
     
+    // TC:  O(nlogn)
+    public int BestApproach(int[] A, int k)
+    {
+        int ans = 0;
+        
+        // PriorityQueue<Integer> maxPQ = new PriorityQueue<>((a,b) -> b - a); 
+        PriorityQueue<Integer> maxPQ = new PriorityQueue<>(Collections.reverseOrder()); 
+        
+        for(int i = 0; i < A.length; i++)
+        {
+            maxPQ.add(A[i]);
+            ans += A[i];
+        }
+            
+        while(k-- != 0)
+        {
+            int num = maxPQ.poll();
+            // num = num - (int)Math.floor(num/2); 
+            maxPQ.add(num - (int)Math.floor(num/2));
+            
+            ans = ans - num/2;
+        }
+
+        return ans;
+    }
+    
+    // TC: O(NlogN) + O(KlogN) = O(NlogN)
     public int ApproachI(int[] A, int k)
     {
         // PriorityQueue<Integer> maxPQ = new PriorityQueue<>((a,b) -> b - a); 
