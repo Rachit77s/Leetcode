d34157342f3bdef97d6a800dc50e1a2f5e300f43
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2022-12-29 21:06:06 +0530

    Time: 138 ms (82.49%), Space: 101 MB (69.59%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.java
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.java
@@ -1,39 +1,82 @@
 class Solution {
     public int[] getOrder(int[][] tasks) {
         
-        int n = tasks.length;
-        int[] ans = new int[n];
-        int[][] extTasks = new int[n][3];
-        for(int i = 0; i < n; i++) {
-            extTasks[i][0] = i;
-            extTasks[i][1] = tasks[i][0];
-            extTasks[i][2] = tasks[i][1];
-        }
-        Arrays.sort(extTasks, (a,b)->a[1] - b[1]);
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> a[2] == b[2] ? a[0] - b[0] : a[2] - b[2]);
-        int time = 0;
-        int ai = 0;
-        int ti = 0;
-        while(ai < n) {
-            while(ti < n && extTasks[ti][1] <= time) {
-                pq.offer(extTasks[ti++]);
-                
-            }
-            if(pq.isEmpty()) {
-                time = extTasks[ti][1];
-                continue;
-            }
-            int[] bestFit = pq.poll();
-            ans[ai++] = bestFit[0];
-            time += bestFit[2];
-        }
-        return ans;
-        
-        //return ApproachI(tasks);
+        return ApproachI(tasks);
     }
     
-    public void ApproachI(int[][] A) 
+    public int[] ApproachI(int[][] tasks) 
     {
+        // 1. Sort by start time but preserve the original index.
+        // 2. Use a minHeap to store current tasks with a custom
+        // 3. Iterate each task, and we increaste the ans time, and if the 
+        // curr task starting time is smaller or equal to cur, we add into the pq
+        // 4. Check if pq is empty, if yes, we update current time by ith task starting time
+        // 5. If pq is not empty, we update the result
+        // 6. clean the minHeap
+        // 7. return result
         
+        
+        // Contains all the tasks, start time, end time and the index
+        int[][] arr = new int[tasks.length][3]; 
+        
+        for(int i = 0; i < tasks.length; i++)
+            arr[i] = new int[]{tasks[i][0], tasks[i][1], i};
+        
+        // Sort arr by start time
+        Arrays.sort(arr, (a, b) -> {
+            if(a[0] != b[0]) 
+                return a[0]-b[0];
+            else 
+                return a[1] - b[1];//least duration come first
+        });
+        
+        // Create heap by pq and sort by duration and start time
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
+           if(a[1] != b[1])
+               return a[1] - b[1];
+            else
+                return a[2] - b[2];//least start time come first
+        });
+        
+        int[] ans = new int[tasks.length];
+        int index = 0; // for maintaining ans index
+        int time = 0;
+        
+        //There are 2 situations:
+        //1:no task can be executed E.g. t = 0, [1,2], queue={}, then t = next start time
+        //2:there are task start time <= t, add task into queue, then 
+        // Pick the task with least duration and update t = t + duration
+        int i = 0;
+        while(i < arr.length)
+        {
+            //add task into heap
+            while(i < arr.length && time >= arr[i][0])
+            {
+                pq.add(arr[i]);
+                i++;
+            }
+                
+    //Situation 1: no task can be executed at time t, update time t = next task's start time
+            if(pq.isEmpty())
+                time = arr[i][0];
+            
+            //Situation 2:pick task, execute task and update t = t + duration
+            if(!pq.isEmpty())
+            {
+                int[] curr = pq.poll();
+                ans[index] = curr[2]; // add index(arr[2]) to ans
+                index++;
+                time += curr[1];
+            }
+        }
+        
+        //Finsh the rest of task in queue
+        while(!pq.isEmpty())
+        {
+            int[] curr = pq.poll();
+            ans[index] = curr[2];
+            index++;
+        }
+        return ans;
     }
 }
