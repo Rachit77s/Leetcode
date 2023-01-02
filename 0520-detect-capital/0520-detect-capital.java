53bf5a8f553d8bafe5ad63a58a8bbee67fc5f53a
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-01-02 18:45:08 +0530

    Time: 1 ms (100.00%), Space: 40.4 MB (95.10%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,6 +1,39 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
         
+        return ApproachI(word);
+    }
+    
+    public boolean ApproachI(String word)
+    {
+        int len = word.length();
+        
+        if (word == null || len == 0) 
+            return false;
+        
+        boolean hasCapital = false;
+        int count = 0;
+        
+        // 1. check whether the word is capitalized or not.
+        if (word.charAt(0)-'Z' <= 0) 
+        {
+            hasCapital = true;
+            count++;
+        }
+        
+        // 2. check other letters
+        for (int i = 1; i < len; i++) 
+        {
+            if (word.charAt(i)-'Z' <= 0) 
+                count++;
+        }
+        
+        // 3. (1) no uppercase (2) Capitalize (3) All uppercases
+        return count == 0 || (hasCapital && (count == 1 || count == len));
+    }
+    
+    public boolean ApproachII(String word)
+    {
         int count = 0;
         int idx = -1;
         
