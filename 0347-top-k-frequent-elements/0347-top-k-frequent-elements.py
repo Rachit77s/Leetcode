6616cc3d47eac171faac9306efacce818793c1fa
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-01-03 23:05:53 +0530

    Time: 258 ms (27.81%), Space: 24.5 MB (5.94%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,12 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        frq = defaultdict(list)
+        for key, cnt in Counter(nums).items():
+            frq[cnt].append(key)
+
+        res = []
+        for times in reversed(range(len(nums) + 1)):
+            res.extend(frq[times])
+            if len(res) >= k: return res[:k]
+
+        return res[:k]
