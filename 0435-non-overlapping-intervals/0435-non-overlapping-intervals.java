667ee1b73e8a9136c85839d3f64950752bf883fb
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-01-06 23:30:32 +0530

    Time: 143 ms (14.53%), Space: 99.9 MB (81.08%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,11 +1,32 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         
+        // return Helper2(intervals);
         return Helper1(intervals);
     }
     
     public int Helper1(int[][] intervals) 
     {
+        /*
+            Draw on number line:
+            
+            [1,6] [3,9]
+            Which one to delete, does order of their deletion matters?
+            Yes, in this case it doesn't appear. But consider below
+            
+            [1,6] [3,9] [7,15]
+            Now deletion matters, hence, we need to delete [3,9]
+            to get ans = 1(No of deletions)
+            
+            Case 2: When 1st interval is larger
+            [1,9] [3,6] [7,10]
+            Order of deletion matters, hence, delete [1,9] to get
+            ans = 1, otherwise, if [1,9] would not be deleted
+            we would had to delete [3,6] [7,10] and ans would be 2.
+            
+        */
+        
+        // Sort by start
         Arrays.sort(intervals, (a,b) -> {
             if(a[0] != b[0]) 
                 return a[0] - b[0];
@@ -13,12 +34,61 @@ class Solution {
                 return a[1] - b[1];
         });
         
-        // [[-52,31],[-73,-26],[82,97],[-65,-11],[-62,-49],[95,99],[58,95],[-31,49],[66,98],[-63,2],[30,47],[-40,-26]]
+        int count = 0;
+        int prevStart = intervals[0][0];
+        int prevEnd = intervals[0][1];
         
-        for (int i = 0; i < intervals.length; i++)
+        for (int i = 1; i < intervals.length; i++)
         {
-            System.out.print(intervals[i][0] + "," + intervals[i][1] + " ");
+            int currStart = intervals[i][0];
+            
+            // [1,5] [3,6] Overlapping intervals
+            // Intervals are overlapping
+            if(prevEnd > currStart)
+            {
+                count++;
+                
+                /*
+                    Now there are 2 cases in overlapping
+                    
+                    Case1: [1,5] [3,6]
+                    We would have deleted bigger end time interval
+                    So delete [3,6]
+                    
+                    Case2: [1,5] [2,3]
+                    We would delete the bigger end time interval
+                    i.e. Delete [1,5]
+                */
+                
+                // currEnd > prevEnd
+                if(intervals[i][1] > prevEnd)
+                {
+                    // Do nothing, so high end time gets ignore
+                }
+                else if(prevEnd >= intervals[i][1])
+                {
+                    // Update prev to current as curr is lower end time
+                    prevEnd = intervals[i][1];
+                }
+            }
+            else
+            {
+                prevEnd = intervals[i][1];
+            }
         }
+        
+        return count;
+    }
+    
+        
+    public int Helper2(int[][] intervals) 
+    {
+        Arrays.sort(intervals, (a,b) -> {
+            if(a[0] != b[0]) 
+                return a[0] - b[0];
+            else
+                return a[1] - b[1];
+        });
         
         int count = 0;
         int prevStart = intervals[0][0];
@@ -33,12 +103,13 @@ class Solution {
             {
                 count++;
                 // Keep the interval which has min end time,
-                // delete the interval with greater end time.
+                // Delete the interval with greater end time.
+                // Because larger interval might overlap with some
+                // other interval.
                 prevEnd = Math.min(prevEnd, intervals[i][1]);
             }
             else
             {
-                // prevStart = currStart;
                 prevEnd = intervals[i][1];
             }
         }
