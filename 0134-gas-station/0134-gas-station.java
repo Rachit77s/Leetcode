03e0c862a4b03e61bbfd769bef4601b8bd94f673
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-01-08 13:06:16 +0530

    Time: 1 ms (100.00%), Space: 62.6 MB (67.95%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,19 +1,50 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int n = gas.length;
-        int total_surplus = 0;
-        int surplus = 0;
-        int start = 0;
+//         int n = gas.length;
+//         int total_surplus = 0;
+//         int surplus = 0;
+//         int start = 0;
         
-        for(int i = 0; i < n; i++){
-            total_surplus += gas[i] - cost[i];
-            surplus += gas[i] - cost[i];
-            if(surplus < 0){
-                surplus = 0;
-                start = i + 1;
+//         for(int i = 0; i < n; i++){
+//             total_surplus += gas[i] - cost[i];
+//             surplus += gas[i] - cost[i];
+//             if(surplus < 0){
+//                 surplus = 0;
+//                 start = i + 1;
+//             }
+//         }
+//         return (total_surplus < 0) ? -1 : start;
+        
+        return Helper1(gas, cost);
+    }
+    
+    // Neetcode: https://www.youtube.com/watch?v=lJwbPZGo05A
+    // Alisha: https://www.youtube.com/watch?v=MWTHTDIBN0s
+    public int Helper1(int[] gas, int[] cost)
+    {
+        // Check if the solution even exists
+        int sum = 0;
+        for(int i = 0; i < gas.length; i++)
+            sum = sum + gas[i] - cost[i];
+        
+        if(sum < 0)
+            return -1;
+        
+        // Solution exists, let's find it.
+        int startIndex = 0;
+        int remainingFuel = 0;
+        for(int i = 0; i < gas.length; i++)
+        {
+            remainingFuel = remainingFuel + gas[i] - cost[i];
+            
+            if(remainingFuel < 0)
+            {
+                startIndex = i + 1;
+                remainingFuel = 0;
             }
         }
-        return (total_surplus < 0) ? -1 : start;
+        
+        return startIndex;  
     }
 
 }
