7d3b18cf2711b2e0a84a237cbb39d8eb1f4c75c2
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-01-18 12:04:55 +0530

    Time: 20 ms (82.02%), Space: 44 MB (56.45%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -6,7 +6,7 @@ class Solution {
     
     public boolean isMatch(String s, String p) {
         
-        // Make int dp arr to boolean for Bottom Up
+
         // return BottomUp(s, p);
         
         if (TopDownHelper(s, p) == 1)
@@ -98,41 +98,41 @@ class Solution {
         // return dp[ls][lp];
     }
     
-//     public boolean BottomUp(String s, String p)
-//     {
-//         dp = new boolean[s.length()+1][p.length()+1];
+    public boolean BottomUp(String s, String p)
+    {
+        boolean[][] dp2 = new boolean[s.length()+1][p.length()+1];
         
-//         int ls = s.length();
-//         int lp = p.length();
+        int ls = s.length();
+        int lp = p.length();
         
-//         // Base case: s & p both are 0
-//         dp[0][0] = true;
+        // Base case: s & p both are 0
+        dp2[0][0] = true;
         
-//         // When the string is empty, check the pattern for *
-//         for (int i = 1; i <= lp ; i++) 
-//         {
-//             if (p.charAt(i-1) == '*') 
-//                 dp[0][i] = true;
-//             else
-//                 break;
-//         }
+        // When the string is empty, check the pattern for *
+        for (int i = 1; i <= lp ; i++) 
+        {
+            if (p.charAt(i-1) == '*') 
+                dp2[0][i] = true;
+            else
+                break;
+        }
         
-//         for (int i = 1; i <= ls; i++) 
-//         {
-//             for (int j = 1; j <= lp; j++) 
-//             {
-//                 if (s.charAt(i-1) == p.charAt(j-1) || 
-//                     p.charAt(j-1) == '?')
-//                 {
-//                     dp[i][j] = dp[i-1][j-1];
-//                 }
-//                 else if (p.charAt(j-1) == '*') 
-//                 {   
-//                      dp[i][j] = dp[i][j-1] | dp[i-1][j];
-//                 } 
-//             }
-//         }
+        for (int i = 1; i <= ls; i++) 
+        {
+            for (int j = 1; j <= lp; j++) 
+            {
+                if (s.charAt(i-1) == p.charAt(j-1) || 
+                    p.charAt(j-1) == '?')
+                {
+                    dp2[i][j] = dp2[i-1][j-1];
+                }
+                else if (p.charAt(j-1) == '*') 
+                {   
+                     dp2[i][j] = dp2[i][j-1] | dp2[i-1][j];
+                } 
+            }
+        }
         
-//         return dp[s.length()][p.length()];
-//     }
+        return dp2[s.length()][p.length()];
+    }
 }
