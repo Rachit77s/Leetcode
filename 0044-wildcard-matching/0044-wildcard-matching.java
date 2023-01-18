e07f09e76a49d18d92cb6ea1b834247cf47e6013
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-01-18 11:52:53 +0530

    Time: 21 ms (80.13%), Space: 43.8 MB (58.80%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,28 +1,47 @@
 class Solution {
 
-    boolean [][]dp;
+    // boolean [][]dp;
+    
+    int[][] dp;
     
     public boolean isMatch(String s, String p) {
         
-        return BottomUp(s, p);
+        // Make int dp arr to boolean for Bottom Up
+        // return BottomUp(s, p);
+        
+        if (TopDownHelper(s, p) == 1)
+            return true;
+        
+        return false;
     }
     
-    public boolean TopDownHelper(String s, String p) 
+    public int TopDownHelper(String s, String p) 
     {
-        dp = new boolean[s.length() + 1][p.length() + 1];
+        dp = new int[s.length() + 1][p.length() + 1];
+        
+        // for (int i = 0; i < s.length() + 1; i++)
+        //     Arrays.fill(dp[i], -1);
+        
+        for (int i = 0; i < s.length() + 1; i++) 
+        {
+            for (int j = 0; j < p.length() + 1; j++) 
+            {
+                dp[i][j] = -1;
+            }
+        }
         
         return IsMatchTopDown(s, s.length(), p, p.length());
     }
     
     // Pass the length of the string
     // l = 0 string is null
-    public boolean IsMatchTopDown(String s, int ls, String p, int lp)
+    public int IsMatchTopDown(String s, int ls, String p, int lp)
     {
         if(ls == 0 && lp == 0)
-            return true;
+            return 1;
         
         if(lp == 0)
-            return false;
+            return 0;
         
         // Tricky case: If string is empty, then
         // check if pattern has all the starts,
@@ -33,25 +52,25 @@ class Solution {
             for(int i = 0; i < lp; i++)
             {
                 if(p.charAt(i) != '*')
-                    return false;
+                    return 0;
             }
             
-            return true;
+            return 1;
         }
         
-        if(dp[ls][lp] != false)
+        if(dp[ls][lp] != -1)
             return dp[ls][lp];
         
         // Case 1: Last char is a lowercase
         // Last char matches
         if(s.charAt(ls - 1) == p.charAt(lp - 1))
         {
-            dp[ls][lp] = IsMatchTopDown(s, ls - 1, p, lp - 1);
+            return dp[ls][lp] = IsMatchTopDown(s, ls - 1, p, lp - 1);
         }
         // Case 2: Last char is a "."
         else if(p.charAt(lp - 1) == '?')
         {
-            dp[ls][lp] = IsMatchTopDown(s, ls - 1, p, lp - 1);
+            return dp[ls][lp] = IsMatchTopDown(s, ls - 1, p, lp - 1);
         }
         // Case 3: Last char is a "*"
         else if(p.charAt(lp - 1) == '*')
@@ -68,52 +87,52 @@ class Solution {
                 IsMatchTopDown(s, ls - 1, p, lp)
             */
             
-            dp[ls][lp] = IsMatchTopDown(s, ls, p, lp - 1) ||
-                         IsMatchTopDown(s, ls - 1, p, lp);
+            return dp[ls][lp] = (IsMatchTopDown(s, ls, p, lp - 1) == 1 ||
+                         IsMatchTopDown(s, ls - 1, p, lp) == 1) ? 1 : 0;
         }
         else
         {
-            dp[ls][lp] = false;
+            return dp[ls][lp] = 0;
         }
         
-        return dp[ls][lp];
+        // return dp[ls][lp];
     }
     
-    public boolean BottomUp(String s, String p)
-    {
-        dp = new boolean[s.length()+1][p.length()+1];
+//     public boolean BottomUp(String s, String p)
+//     {
+//         dp = new boolean[s.length()+1][p.length()+1];
         
-        int ls = s.length();
-        int lp = p.length();
+//         int ls = s.length();
+//         int lp = p.length();
         
-        // Base case: s & p both are 0
-        dp[0][0] = true;
+//         // Base case: s & p both are 0
+//         dp[0][0] = true;
         
-        // When the string is empty, check the pattern for *
-        for (int i = 1; i <= lp ; i++) 
-        {
-            if (p.charAt(i-1) == '*') 
-                dp[0][i] = true;
-            else
-                break;
-        }
+//         // When the string is empty, check the pattern for *
+//         for (int i = 1; i <= lp ; i++) 
+//         {
+//             if (p.charAt(i-1) == '*') 
+//                 dp[0][i] = true;
+//             else
+//                 break;
+//         }
         
-        for (int i = 1; i <= ls; i++) 
-        {
-            for (int j = 1; j <= lp; j++) 
-            {
-                if (s.charAt(i-1) == p.charAt(j-1) || 
-                    p.charAt(j-1) == '?')
-                {
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                else if (p.charAt(j-1) == '*') 
-                {   
-                     dp[i][j] = dp[i][j-1] | dp[i-1][j];
-                } 
-            }
-        }
+//         for (int i = 1; i <= ls; i++) 
+//         {
+//             for (int j = 1; j <= lp; j++) 
+//             {
+//                 if (s.charAt(i-1) == p.charAt(j-1) || 
+//                     p.charAt(j-1) == '?')
+//                 {
+//                     dp[i][j] = dp[i-1][j-1];
+//                 }
+//                 else if (p.charAt(j-1) == '*') 
+//                 {   
+//                      dp[i][j] = dp[i][j-1] | dp[i-1][j];
+//                 } 
+//             }
+//         }
         
-        return dp[s.length()][p.length()];
-    }
+//         return dp[s.length()][p.length()];
+//     }
 }
