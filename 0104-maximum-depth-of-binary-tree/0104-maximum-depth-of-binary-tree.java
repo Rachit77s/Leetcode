ad335f8a48582636054db5c7cfc22accbe5305ca
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-02-16 15:57:13 +0530

    Time: 1 ms (12.92%), Space: 42.3 MB (42.92%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public int maxDepth(TreeNode root) {
+         return Iterative(root);
+    }
+    
+    public int Iterative(TreeNode root)
+    {
+        if(root == null)
+            return 0;
+        
+        int height = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        
+        while(q.size() != 0)
+        {
+            height = height + 1;
+            int levelSize = q.size();
+            
+            for(int i = 0; i < levelSize; i++)
+            {
+                TreeNode temp = q.remove();
+                
+                if(temp.left != null)
+                    q.add(temp.left);
+                
+                if(temp.right != null)
+                    q.add(temp.right);
+            }
+        }
+        
+        return height;
+    }
+    
+    public int Recursive(TreeNode root)
+    {
+        if(root == null)
+            return 0;
+        
+        int leftHeight = Recursive(root.left);
+        int rightHeight = Recursive(root.right);
+        
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
