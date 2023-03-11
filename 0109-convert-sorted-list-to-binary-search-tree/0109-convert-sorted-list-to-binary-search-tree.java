1897479383e46416d6e3751c52e849a79d55cccb
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-03-11 23:18:54 +0530

    Time: 0 ms (100.00%), Space: 44 MB (16.95%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
@@ -26,7 +26,50 @@
 class Solution {
     public TreeNode sortedListToBST(ListNode head) {
         
-        return Helper(head);
+        return OptimizedApproach(head);
+        // return Helper(head);
+    }
+    
+    public TreeNode OptimizedApproach(ListNode head) 
+    {
+    /*
+        Steps:
+        1. Find the middle element of the LL
+        2. Break the link after the mid
+    */
+        
+        if(head == null)
+            return null;
+        
+        if(head.next == null)
+            return new TreeNode(head.val);
+        
+//         // Find middle
+//         // Get middle element by Floyd's Cycle Detection Algorithm.
+// 		ListNode fast = head;
+// 		ListNode slow = head;
+// 		while(fast != null && fast.next != null)
+// 		{
+// 				slow = slow.next;      // move by x speed
+// 				fast = fast.next.next; // move by 2x speed
+// 		}
+
+// 		// If fast is null, it means it is an even length LL
+// 		// If fast is not null, it means it is a odd length LL
+// 		if(fast != null)
+// 			 slow = slow.next; // Move slow to one ahead
+        
+        ListNode slow=head,fast=head,prev=null;
+            while(fast!=null&&fast.next!=null){
+                prev=slow;
+                fast=fast.next.next;
+                slow=slow.next;
+            }
+        prev.next=null;
+        TreeNode root=new TreeNode(slow.val);
+        root.left=sortedListToBST(head);
+        root.right=sortedListToBST(slow.next);
+        return root;
     }
     
     public TreeNode Helper(ListNode head)
