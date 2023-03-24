834df28db65af588fb0a30fd2864b77268c423a1
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-03-25 00:28:17 +0530

    Time: 53 ms (5.07%), Space: 50.3 MB (16.94%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
@@ -1,7 +1,170 @@
 class Solution {
     public int minFlipsMonoIncr(String s) {
         
-        return Helper(s);
+        return Optimized2(s);
+        // return BruteForce(s);
+        // return Helper(s);
+    }
+    
+    // Subham: https://www.scaler.com/help_requests/662642/?ref=join-call-now
+    public int BruteForce(String s)
+    {
+        // To make string monotonic increasing, at every index
+        // We need to make all the, 1's at the left of the index to 0
+        // And, all the 0's to the right of the index's to 1
+        // The current char sign for whom we are calculating doesn't matter 
+        // Because consider,  11011   so for 0 we will make left to it 0
+        // And right to it as 1, and 0 wouldn't matter at all.
+        
+        int n = s.length();
+        // int leftOnes = 0;
+        // int rightZeroes = 0;
+        int ans = Integer.MAX_VALUE;
+
+        for(int index = 0; index < n; index++)
+        {
+            int leftOnes = 0;
+            int rightZeroes = 0;
+            
+            for(int leftIdx = index - 1; leftIdx >= 0; leftIdx--)
+            {
+                if(s.charAt(leftIdx) == '1')
+                    leftOnes++;
+            }
+            
+            for(int rightIdx = index + 1; rightIdx < n; rightIdx++)
+            {
+                if(s.charAt(rightIdx) == '0')
+                    rightZeroes++;
+            }
+            
+            // The no of 1's on the left + no of 0's on right
+            // together gives us the flips required
+            ans = Math.min(ans, leftOnes + rightZeroes);
+        }
+        
+        return ans;
+    }
+    
+    public int OptimizedApproach(String s)
+    {
+        int n = s.length();
+        int ans = Integer.MAX_VALUE;
+        
+        // Initialize two lists to store the number of 
+        // 1s on the left and the number of 0s on the right
+        // Calculate prefix 1's
+//         int[] prefixOnes = new int[n];
+//         if(s.charAt(0) == '1')
+//             prefixOnes[0] = 1;
+//         else
+//             prefixOnes[0] = 0;
+        
+//         for(int i = 1; i < n; i++)
+//         {
+//             if(s.charAt(i) == '1')
+//                 prefixOnes[i] = prefixOnes[i - 1] + 1;
+//             else
+//                 prefixOnes[i] = prefixOnes[i - 1] + 0;
+//         }
+        
+//         // Calculate suffix 0's
+//         int[] suffixZeroes = new int[n];
+//         if(s.charAt(n-1) == '0')
+//             suffixZeroes[0] = 1;
+//         else
+//             suffixZeroes[0] = 0;
+        
+//         for(int i = n - 2; i >= 0; i--)
+//         {
+//             if(s.charAt(i) == '0')
+//                 suffixZeroes[i] = suffixZeroes[i + 1] + 1;
+//             else
+//                 suffixZeroes[i] = suffixZeroes[i + 1] + 0;
+//         }
+        
+        
+        
+        int[] prefixOnes = new int[n];
+        prefixOnes[0] = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(s.charAt(i) == '1')
+                prefixOnes[i] = prefixOnes[i] + 1;
+            else
+                prefixOnes[i] = prefixOnes[i] + 0;
+        }
+        
+        // Calculate suffix 0's
+        int[] suffixZeroes = new int[n];
+        suffixZeroes[0] = 0;
+        
+        for(int i = n - 1; i >= 0; i--)
+        {
+            if(s.charAt(i) == '0')
+                suffixZeroes[i] = suffixZeroes[i] + 1;
+            else
+                suffixZeroes[i] = suffixZeroes[i] + 0;
+        }
+        
+        for(int index = 0; index < n; index++)
+        {
+            // We need prefix sum till the curr index
+            int leftOnes = prefixOnes[index];
+            // We need suffix sum from the curr index
+            int rightZeroes = suffixZeroes[index + 1];
+            
+            ans = Math.min(ans, leftOnes + rightZeroes);
+        }
+        
+        return ans;
+    }
+    
+    
+    public int Optimized2(String s)
+    {
+        // Initialize two lists to store the number of 1s on the left and the number of 0s on the right
+        List<Integer> leftOnes = new ArrayList<>();
+        List<Integer> rightZeroes = new ArrayList<>();
+
+        // Add a 0 to the leftOnes list to represent the left side of the first character
+        leftOnes.add(0);
+
+        // Loop through each character in the string
+        for (char c : s.toCharArray()) {
+            // If the character is a 1, add 1 to the last element in the leftOnes list and append it to the list
+            // Otherwise, add 0 to the last element in the leftOnes list and append it to the list
+            leftOnes.add(leftOnes.get(leftOnes.size() - 1) + (c == '1' ? 1 : 0));
+        }
+
+        // Add a 0 to the rightZeroes list to represent the right side of the last character
+        rightZeroes.add(0);
+
+        // Loop through the string backwards
+        for (int i = s.length() - 1; i >= 0; i--) {
+            // If the character is a 0, add 1 to the last element in the rightZeroes list and append it to the list
+            // Otherwise, add 0 to the last element in the rightZeroes list and append it to the list
+            rightZeroes.add(rightZeroes.get(rightZeroes.size() - 1) + (s.charAt(i) == '0' ? 1 : 0));
+        }
+
+        // Reverse the rightZeroes list so that it corresponds to the correct characters in the string
+        Collections.reverse(rightZeroes);
+
+        // Initialize a variable to store the minimum number of flips
+        int minFlips = Integer.MAX_VALUE;
+
+        // Loop through each index in the string
+        for (int i = 0; i < s.length(); i++) {
+            // Get the number of 1s on the left and the number of 0s on the right
+            int leftOnesCount = leftOnes.get(i);
+            int rightZeroesCount = rightZeroes.get(i + 1);
+            // Update the minimum number of flips if the current combination requires fewer flips
+            minFlips = Math.min(minFlips, leftOnesCount + rightZeroesCount);
+        }
+
+        // Return the minimum number of flips
+        return minFlips;
     }
     
     // Very hard question
@@ -26,6 +189,7 @@ class Solution {
             // 1. keep it as 0, and flip all the preceeding 1 to 0, need to know the count of ones so far
             // 2. flip it to 1, will not need to do anything    
         */
+ 
         
         for(int i = 0; i < s.length(); i++)
         {
