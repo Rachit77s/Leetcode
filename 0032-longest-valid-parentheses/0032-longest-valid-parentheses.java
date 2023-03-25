eb55d132042c61c82d5561aa81d7d4d50cc53417
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-03-25 17:36:17 +0530

    Time: 5 ms (64.82%), Space: 42.7 MB (23.83%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,8 +1,81 @@
 class Solution {
     public int longestValidParentheses(String s) {
         
-        return Helper(s);
+        return UnderstandableApproach(s);
+        // return Help(s);
+        // return Helper(s);
     }
+    
+    // https://www.scaler.com/help_requests/662642/?ref=join-call-now
+    public int UnderstandableApproach(String s)
+    {
+        // 51:00
+        // We cannot figure out which opening bracket is invalid
+        // But we can always say which closing bracket will be invalid
+        // So we will find the invalid index
+        // We will find invalid index from the left
+        // As well as from the right
+        
+        // Scan from the left & add the invalid index in set
+        // Scan from the right & add the invalid index in set
+        HashSet<Integer> invalidIndexSet = new HashSet<>();
+        
+        int balCount = 0;
+        
+        // Scan from the left for opening bracket
+        for(int i = 0; i < s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            
+            if(ch == '(')
+                balCount += 1;
+            else
+            {
+                // We found a valid pair
+                if(balCount > 0)
+                    balCount -= 1;
+                else
+                    // Opening with no closing bracket
+                    invalidIndexSet.add(i); // Add invalid index
+            }
+        }
+        
+        balCount = 0;
+        // Scan from the right for closing bracket
+        for(int i = s.length() - 1; i >= 0; i--)
+        {
+            char ch = s.charAt(i);
+            
+            if(ch == ')')
+                balCount += 1;
+            else
+            {
+                // We found a valid pair
+                if(balCount > 0)
+                    balCount -= 1;
+                else
+                    // Opening with no closing bracket
+                    invalidIndexSet.add(i); // Add invalid index
+            }
+        }
+        
+        
+        int maxAns = 0;
+        int ans = 0;
+        for(int i = 0; i < s.length(); i++)
+        {
+            // At invalid index, set ans to 0
+            if(invalidIndexSet.contains(i))
+                ans = 0;
+            else
+                ans += 1;
+            
+            maxAns = Math.max(maxAns, ans);
+        }
+        
+        return maxAns;
+    }
+    
     
     public int Helper(String A)
     {
