c642e4c51dabc390edce46a1528cefb571f5b60b
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-03-29 10:54:33 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (52.00%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,9 +1,34 @@
-class Solution:
-    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        dp=[0 for i in range(days[-1]+1)]
-        for i in range(days[-1]+1):
-             if i not in days:
-                dp[i]=dp[i-1]
-             else:
-                dp[i]=min(dp[max(0,i-7)]+costs[1],dp[max(0,i-1)]+costs[0],dp[max(0,i-30)]+costs[2])
-        return dp[-1]
+class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+        
+        if(days==null || days.length==0)
+            return 0;
+        
+        int n = days.length;
+        
+        int[] dp = new int[days.length+1];
+        
+        return func(days, costs, n, 0, dp);
+    }
+    
+    public int func(int[] days, int[] costs, int n, int idx, int[] dp)
+    {
+        if(idx >= n)
+            return 0;
+        
+        if(dp[idx] != 0)
+            return dp[idx];
+        
+        int a = costs[0] + func(days, costs, n, idx + 1, dp);
+        int i;
+        for(i=idx; i<n && days[i]<days[idx]+7;i++);
+        int b=costs[1]+func(days, costs, n, i, dp);
+        for(i=idx;i<n && days[i]<days[idx]+30;i++);
+        int c=costs[2]+func(days, costs, n, i, dp);
+        
+        dp[idx]=Math.min(a, Math.min(b, c));
+        
+        return dp[idx];
+            
+    }
+}
