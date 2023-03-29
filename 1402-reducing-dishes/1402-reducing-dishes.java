e78dc2c041f68c0bb4c471898f6d5cf1cd84a834
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-03-29 23:41:57 +0530

    Time: 21 ms (9.02%), Space: 43.1 MB (12.63%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -1,15 +1,21 @@
 class Solution {
-    public int maxSatisfaction(int[] satisfaction) {
+    public int maxSatisfaction(int[] A) {
         
-        return Helper1(satisfaction);
-        // return RecursionHelper(satisfaction);
+        return TopDownRecursionHelperFunction(A);
     }
     
-    public int RecursionHelper(int[] A) 
+    
+    // Babbar: https://www.youtube.com/watch?v=_iGlRDLPLxM
+    public int TopDownRecursionHelperFunction(int[] A) 
     {
+        // Question is similar to include/exclude approach
+        // Similar to Knapsack
+        
+        // Sort the array
         Arrays.sort(A);
         
         int index = 0;
+        
         int[][] dp = new int[A.length + 1][A.length + 1];
         
         for(int i = 0; i <= A.length; i++)
@@ -20,10 +26,11 @@ class Solution {
             }
         }
         
-        return TopDownRecursion(A, index, 0, dp);
+        int time = 1;
+        return TopDownRecursion(A, index, time, dp);
     }
     
-    public int TopDownRecursion(int[] A, int index, int time, int[][] dp)
+    public int TopDownRecursion(int[] A, int index, int time, int[][] dp) 
     {
         if(index == A.length)
             return 0;
@@ -31,54 +38,27 @@ class Solution {
         if(dp[index][time] != -1)
             return dp[index][time];
         
-        int include = A[index] * (time+1) + TopDownRecursion(A, index + 1, time + 1, dp);
+        int include = A[index] * (time) + 
+                    TopDownRecursion(A, index + 1, time + 1, dp);
         
         int exclude = TopDownRecursion(A, index + 1, time, dp);
         
         return dp[index][time] = Math.max(include, exclude);
     }
     
-    public int Helper1(int[] A) 
-    {
-        Arrays.sort(A);
+    /*
+        [-1,-2,-3,5,2] --> [-3,-2,-1,2,5]
+        [-10,-80,100,5]
+        [-100,-8,25]
+    */
+    
+    // faster
+    // https://www.youtube.com/watch?v=mB24wDI0Bp0
+//     public int Approach2(int[] A) 
+//     {
         
-        int currSum = 0;
-        int startIdx = A.length - 1;
-        
-        for(int i = A.length - 1; i >= 0; i--)
-        {
-            currSum += A[i];
-            
-            if(currSum < 0)
-                break;
-            
-            startIdx--;
-        }
-        
-        startIdx++;
-        
-        int time = 1;
-        
-        int ans = 0;
-        for(int i = startIdx; i < A.length; i++)
-        {
-            ans += A[i] * time;
-            time++;
-        }
-        
-        return ans;
-    }
+//     }
 }
 
-// -1 -2 -3 5 2
 
-/*
 
-[-1,-2,-3,5,2]
-
-[-3,-2,-1,2,5] : 23     25+8-3-4-3
-
-[-10,-80,100,5]: -80-20+15+400
-[-100,-8,25]
-
-*/
