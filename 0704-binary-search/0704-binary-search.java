6ea9ab5d21d29edcef0d74f27127a8318d82e7e8
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-04-03 23:39:53 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (51.76%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,6 +1,11 @@
 class Solution {
     public int search(int[] nums, int target) {
         
+        // return Template1(nums, target);
+        return Template2(nums, target);
+    }
+    
+    public int Template1(int[] nums, int target) {
         
         int left = 0;
         int right = nums.length - 1;
@@ -19,5 +24,25 @@ class Solution {
             return left;
         
         return -1;
+    }
+    
+    // https://www.youtube.com/watch?v=UZx_DI4z_l8
+    // https://leetcode.com/problems/binary-search/discuss/3363888/Image-Explanation-MOST-Generalized-Binary-Search-C%2B%2BJavaPython
+    public int Template2(int[] nums, int target) 
+    {
+        int l = 0, r = nums.length - 1;
+        
+        while (l < r) 
+        {
+            int mid = l + (r - l) / 2;
+            
+            if (nums[mid] >= target)
+                r = mid;
+            else
+                l = mid + 1;  
+        }
+
+        if (nums[l] != target) return -1;
+        return l;
     }
 }
