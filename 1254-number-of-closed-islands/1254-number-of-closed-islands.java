196ec067bea4d819d95f68efd419e28c3c9675c0
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-04-06 22:06:53 +0530

    Time: 3 ms (30.78%), Space: 43.6 MB (11.94%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,19 +1,48 @@
 class Solution {
-int fill(int[][] g, int i, int j) {
-  if (i < 0 || j < 0 || i >= g.length || j >= g[i].length || g[i][j] != 0)
-    return 0;
-  return (g[i][j] = 1) + fill(g, i + 1, j) + fill(g, i, j + 1)
-    + fill(g, i - 1, j) + fill(g, i, j - 1);
-}
-public int closedIsland(int[][] g) {
-  for (int i = 0; i < g.length; ++i)
-    for (int j = 0; j < g[i].length; ++j)
-      if (i * j * (i - g.length + 1) * (j - g[i].length + 1) == 0)
-        fill(g, i, j);
-  int res = 0;
-  for (int i = 0; i < g.length; ++i)
-    for (int j = 0; j < g[i].length; ++j)
-      res += fill(g, i, j) > 0 ? 1 : 0;
-  return res;
-}
+    public int closedIsland(int[][] grid) {
+        
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visit = new boolean[m][n];
+        int count = 0;
+        
+        for (int i = 0; i < grid.length; i++) 
+        {
+            for (int j = 0; j < grid[0].length; j++) 
+            {
+                if (grid[i][j] == 0 && !visit[i][j] && dfs(i, j, m, n, grid, visit)) 
+                {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+
+    public boolean dfs(int x, int y, int m, int n, int[][] grid, boolean[][] visit) 
+    {
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) {
+            return false;
+        }
+        
+        if (grid[x][y] == 1 || visit[x][y]) {
+            return true;
+        }
+
+        visit[x][y] = true;
+        boolean isClosed = true;
+        int[] dirx = {0, 1, 0, -1};
+        int[] diry = {-1, 0, 1, 0};
+
+        for (int i = 0; i < 4; i++) {
+            int r = x + dirx[i];
+            int c = y + diry[i];
+            if (!dfs(r, c, m, n, grid, visit)) {
+                isClosed = false;
+            }
+        }
+
+        return isClosed;
+    }
 }
