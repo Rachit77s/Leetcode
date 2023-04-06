225fc6d4bf6af4d1bd536b9f38d9bbd4155dbd1b
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-04-06 12:57:47 +0530

    Time: 9 ms (76.53%), Space: 62.3 MB (25.63%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
@@ -1,13 +1,48 @@
 class Solution {
     public int minimizeArrayValue(int[] A) {
-        long sum = 0, res = 0;
-        for (int i = 0; i < A.length; ++i) {
-            sum += A[i];
-            res = Math.max(res, (sum + i) / (i + 1));
-        }
-        return (int)res;
+        
+        return NeetcodeApproach(A);
     }
     
+    public int NeetcodeApproach(int[] A)
+    {
+        // Neetcode: https://www.youtube.com/watch?v=AeHMvcKuR0Y 
     
-    
+    /*
+        Observation 1: 
+        The result/ans can't be possibly smaller than the first value in the array.
+        Example1:           0  1  2  3
+                            3  7  1  6
+        prefix sum:         3 10 11 17
+        Avg till ith index: 3 5  4   5  Take Ceil here
+                    
+        
+        Example2:
+                            0  1  2   3
+                            4  3  8   6
+        prefix sum:         4  7  15  21
+        Avg till ith index: 4  4  5   6  Take Ceil here
+        
+        
+        Example3:
+                            0  1  2   3  4
+                            4  3  1   1  6
+        prefix sum:         4  7  8   9  15
+        Avg till ith index: 4  4  3   3  3   Take Ceil here
+        
+    */
+        
+        long sum = A[0], ans = A[0];
+        
+        for(int i = 1; i < A.length; i++)
+        {
+            sum = sum + A[i];
+            
+            int ceilValue = (int)Math.ceil(sum / (i+1.0));
+            
+            ans = Math.max(ans, ceilValue);
+        }
+        
+        return (int)ans;
+    }
 }
