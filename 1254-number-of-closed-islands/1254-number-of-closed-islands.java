d314e6502629bc805a34c5be5b16caad353c3ad1
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-04-06 22:08:22 +0530

    Time: 2 ms (82.36%), Space: 42.9 MB (61.71%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,48 +1,43 @@
 class Solution {
     public int closedIsland(int[][] grid) {
         
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] visit = new boolean[m][n];
-        int count = 0;
+        int m = grid.length; // number of rows in the grid
+        int n = grid[0].length; // number of columns in the grid
+        int count = 0; // counter to keep track of number of closed islands
+        boolean[][] visited = new boolean[m][n]; // to keep track of visited cells
         
-        for (int i = 0; i < grid.length; i++) 
-        {
-            for (int j = 0; j < grid[0].length; j++) 
-            {
-                if (grid[i][j] == 0 && !visit[i][j] && dfs(i, j, m, n, grid, visit)) 
-                {
-                    count++;
+        // loop through all cells, skipping the border cells
+        for (int i = 1; i < m - 1; i++) {
+            for (int j = 1; j < n - 1; j++) {
+                if (grid[i][j] == 0 && !visited[i][j]) { // if this is an unvisited land cell
+                    boolean isClosed = dfs(grid, visited, i, j); // check if it is a closed island
+                    if (isClosed) {
+                        count++; // increment the counter if it is a closed island
+                    }
                 }
             }
         }
-        
-        return count;
+        return count; // return the number of closed islands
     }
-
-    public boolean dfs(int x, int y, int m, int n, int[][] grid, boolean[][] visit) 
-    {
-        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) {
+    
+    private boolean dfs(int[][] grid, boolean[][] visited, int i, int j) {
+        int m = grid.length; // number of rows in the grid
+        int n = grid[0].length; // number of columns in the grid
+        if (i < 0 || i >= m || j < 0 || j >= n) { // if out of bounds, not a closed island
             return false;
         }
-        
-        if (grid[x][y] == 1 || visit[x][y]) {
+        if (visited[i][j]) { // if already visited, not a closed island
             return true;
         }
-
-        visit[x][y] = true;
-        boolean isClosed = true;
-        int[] dirx = {0, 1, 0, -1};
-        int[] diry = {-1, 0, 1, 0};
-
-        for (int i = 0; i < 4; i++) {
-            int r = x + dirx[i];
-            int c = y + diry[i];
-            if (!dfs(r, c, m, n, grid, visit)) {
-                isClosed = false;
-            }
+        visited[i][j] = true; // mark as visited
+        if (grid[i][j] == 1) { // if water, not a closed island
+            return true;
         }
-
-        return isClosed;
+        boolean isClosed = true; // flag to check if all adjacent cells are water (closed island)
+        isClosed &= dfs(grid, visited, i - 1, j); // check the cell to the left
+        isClosed &= dfs(grid, visited, i + 1, j); // check the cell to the right
+        isClosed &= dfs(grid, visited, i, j - 1); // check the cell above
+        isClosed &= dfs(grid, visited, i, j + 1); // check the cell below
+        return isClosed; // return whether all adjacent cells are water (closed island)
     }
 }
