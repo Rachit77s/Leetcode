0d4ddd3c7c7a0135129e12fd060263d3f6cdbfdd
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-04-09 16:35:40 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (33.63%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double myPow(double x, int n) {
+        
+//         double ans = 1;
+        
+//         while(n -- != 0)
+//         {
+//             ans = ans * x;
+//         }
+        
+        return Math.pow(x,n);
+        
+        
+    }
+}
