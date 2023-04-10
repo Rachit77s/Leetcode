9a2abe1051ea9db380b473eb9a72930545155844
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-04-10 21:59:38 +0530

    Time: 11 ms (73.36%), Space: 44.6 MB (56.05%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -9,154 +9,83 @@
  */
 
 
-// Striver: https://www.youtube.com/watch?v=-YbXySKJsX8
 public class Codec {
 
-    private static final String splitter = " ";
+    private static final String spliter = ",";
     private static final String NN = "X";
+    
+    // https://www.scaler.com/help_requests/686140/?ref=join-call-now
     
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
         
-        return QueueSerialize(root);
+        return DFSHelper(root);    
     }
     
-    public String QueueSerialize(TreeNode root)
+    public String DFSHelper(TreeNode root)
+    {
+        StringBuilder sb = new StringBuilder();
+        DFS(root, sb);
+        
+        System.out.print(sb);
+        return sb.toString();
+    }
+    
+    public void DFS(TreeNode root, StringBuilder sb)
     {
         if(root == null)
         {
-            return "";
+            sb.append(NN);
+            sb.append(spliter);
+            return;
         }
         
-        StringBuilder sb = new StringBuilder();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
+        sb.append(root.val);
+        sb.append(spliter);
         
-        while(!q.isEmpty())
-        {
-            TreeNode node = q.poll();
-            
-            if(node == null)
-            {
-                sb.append(NN).append(splitter);
-                continue;
-            }
-            
-            sb.append(node.val).append(splitter);
-            q.add(node.left);
-            q.add(node.right);
-        }
-        
-        return sb.toString();
+        DFS(root.left, sb);
+        DFS(root.right, sb);
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
         
-        return QueueDeserialize(data);
+         return DFSDeserializeHelper(data);
     }
     
-    public TreeNode QueueDeserialize(String data)
+    public TreeNode DFSDeserializeHelper(String data)
     {
-        if(data == "")
+        String[] str = data.split(",");
+        //System.out.println();
+        
+        Queue<String> queue = new LinkedList<>();
+        for(int i = 0; i < str.length; i++)
+            queue.add(str[i]);
+        
+        return DFSDeserialize(queue);
+    }
+    
+    public TreeNode DFSDeserialize(Queue<String> queue)
+    {
+        if(queue.isEmpty())
+            return null;
+        
+        String str = queue.poll();
+            
+        if(str.equals(NN))
         {
             return null;
         }
+            
+        TreeNode root = new TreeNode(Integer.valueOf(str));
+        root.left = DFSDeserialize(queue);
+        root.right = DFSDeserialize(queue);        
         
-        String[] values = data.split(splitter);
-        Queue<TreeNode> q = new LinkedList<>();
-        
-        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
-        q.add(root);
-        
-        for (int i = 1; i < values.length; i++) {
-            TreeNode parent = q.poll();
-            if (!values[i].equals(NN)) {
-                TreeNode left = new TreeNode(Integer.parseInt(values[i]));
-                parent.left = left;
-                q.add(left);
-            }
-            if (!values[++i].equals(NN)) {
-                TreeNode right = new TreeNode(Integer.parseInt(values[i]));
-                parent.right = right;
-                q.add(right);
-            }
-        }
         return root;
     }
+
 }
 
-// Neetcode
-// public class Codec {
-
-//     private static final String spliter = ",";
-//     private static final String NN = "X";
-    
-//     // Preorder Approach: Neetcode : https://www.youtube.com/watch?v=u4JAi2JJhI8
-    
-//     // Encodes a tree to a single string.
-//     public String serialize(TreeNode root) {
-        
-//         return DFSHelper(root);
-//     }
-    
-//     public String DFSHelper(TreeNode root)
-//     {
-//         StringBuilder sb = new StringBuilder();
-//         DFS(root, sb);
-        
-//         return sb.toString();
-//     }
-    
-//     public void DFS(TreeNode root, StringBuilder sb)
-//     {
-//         if(root == null)
-//         {
-//             sb.append(NN).append(spliter);
-//             return;
-//         }
-        
-//         sb.append(root.val).append(spliter);
-        
-//         DFS(root.left, sb);
-//         DFS(root.right, sb);
-//     }
-
-//     // Decodes your encoded data to tree.
-//     public TreeNode deserialize(String data) {
-        
-//         return DFSDeserializeHelper(data);
-//     }
-    
-//     public TreeNode DFSDeserializeHelper(String data)
-//     {
-//         String[] node = data.split("[,]");
-//         int[] index = new int[1];
-        
-//         return DFSDeserialize(node, index);
-//     }
-    
-//     public TreeNode DFSDeserialize(String[] node, int[] index)
-//     {
-//         // if(index[0] == node.length)
-//         //     return null;
-        
-//         if(node[index[0]].equals(NN))
-//         {
-//             index[0]++;
-//             return null;
-//         }
-        
-        
-//         TreeNode root = new TreeNode(Integer.valueOf( node[ index[0] ] ));
-//         index[0]++;
-        
-//         root.left = DFSDeserialize(node, index);
-//         root.right = DFSDeserialize(node, index);
-        
-//         return root;
-//     }
-// }
 
 // Your Codec object will be instantiated and called as such:
 // Codec ser = new Codec();
