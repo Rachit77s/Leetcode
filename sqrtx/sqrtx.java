23d5f7398979dfe7b554b3e7c0c44b3fa67854e9
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-04-22 19:18:30 +0530

    Time: 1 ms, Memory: 39.8 MB - LeetHub

--- sqrtx/sqrtx.java
+++ sqrtx/sqrtx.java
@@ -3,37 +3,19 @@ class Solution {
         
         // Search from 1 to x/2 for linear search
         
-        if (0 == x) 
-            return 0;
-        int left = 1, right = x;
-        int ans = 0;
+        if(x == 0 || x == 1)
+            return x;
+        int left = 1; 
+        int right = x/2 +2;
         
-        while (left <= right) 
-        {
-            int mid = left + (right - left) / 2;
-            
-            if (mid <= x / mid) {
-                left = mid + 1;
-                ans = mid;
-            } else {
+        while (left < right) {            
+            int mid = left + (right-left+1)/2;            
+            if (mid > x/mid) {
                 right = mid - 1;
             }
-        }
-        return ans;
+            else left = mid;
+        }        
+        return left;
         
-//         long left = 1;
-//         long right = x;
-        
-//         while(left < right)
-//         {
-//             long mid = left + (right - left) / 2;
-            
-//             if(mid * mid >= x)
-//                 right = mid;
-//             else
-//                 left = mid + 1;
-//         }
-        
-//         return (int)left;
     }
 }
