b401e0156fd8d423e4b76b8995062f17415db3e7
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-06-11 18:54:22 +0530

    Time: 218 ms (77.93%), Space: 27.8 MB (26.90%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,8 +1,24 @@
-class Solution {
-public:
-    void rotate(vector<int>& nums, int k) {
-vector<int> numcopy = nums;
-        for(int i=0;i<nums.size();i++) 
-            nums[(i+k)%nums.size()]=numcopy[i];
-    }
-};
+class Solution:
+    def reverse (self, nums, i, j) : 
+        li = i
+        ri = j
+        
+        while li < ri:
+            temp = nums[li]
+            nums[li] = nums[ri]
+            nums[ri] = temp
+            
+            li += 1
+            ri -= 1
+            
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        if k < 0 : 
+            k += len(nums)
+        
+        self.reverse(nums,0,len(nums)-1)
+        self.reverse(nums,0, k-1)
+        self.reverse(nums,k, len(nums)-1)
