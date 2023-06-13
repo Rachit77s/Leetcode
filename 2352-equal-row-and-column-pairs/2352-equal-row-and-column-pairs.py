0df93fc78ef72f37a6620a8ae31856d823daa07a
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-06-13 19:10:38 +0530

    Time: 463 ms (97.17%), Space: 21.7 MB (41.09%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,12 +1,17 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        n = len(grid)
+        row_counts = defaultdict(int)
         count = 0
 
-        for i in range(n):
-            for j in range(n):
-                # Compare the row (i-th row) and column (j-th column) for equality
-                if grid[i] == [row[j] for row in grid]:
-                    count += 1
-
+        
+        for row in grid:
+            #print(row)
+            #print(tuple(row))
+            row_counts[tuple(row)] += 1
+        
+        # By using zip(*grid) in the code, it allows iterating over the columns of the grid rather than the rows. 
+        for column in zip(*grid):
+            #print(column)
+            count += row_counts[column]
+            
         return count
