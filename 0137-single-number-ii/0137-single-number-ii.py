762a8f676b25051f0a44eb96a04a601b83d4c258
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-07-04 13:05:34 +0530

    Time: 87 ms (38.02%), Space: 18.1 MB (61.98%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -16,4 +16,10 @@ class Solution:
             
             return ans
         
-        return hashMap(nums)
+        # return hashMap(nums)
+    
+        a = b = 0
+        for n in nums:
+            b = (b^n)&~a
+            a = (a^n)&~b
+        return b
