1619792d0c5889d02894244fc5efc69f2e3c9578
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-08-04 18:59:50 +0530

    Time: 2 ms (88.87%), Space: 43.3 MB (6.33%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,109 +1,100 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
         
-        return RottenOrangesHelper(grid);
+        return RottenOranges(grid);
     }
     
-    class Coordinates
+    private static final int FRESH = 1;
+    private static final int ROTTEN = 2;
+
+    int[] DIRx = new int[] { -1, 0, 1,  0 };
+    int[] DIRy = new int[] {  0, 1, 0, -1 };
+
+    public int RottenOranges(int[][] A)
     {
-        public int X;
-        public int Y;
-        
-        public Coordinates(int i, int j)
+        if(A.length == 0)
+            return 0;
+
+        int R = A.length;
+        int C = A[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        int ans = 0;
+
+        // Push all the rotten oranges for the BFS i.e. Multi-source BFS
+        for(int i = 0; i < R; i++)
         {
-            this.X = i;
-            this.Y = j;
+            for(int j = 0; j < C; j++)
+            {
+                if(A[i][j] == ROTTEN)
+                    queue.add(new int[] {i, j, 0}); // r, c , time
+            }
         }
-    }
-
-    int[] globalRows = new int[] { -1, 0, 1,  0 };
-    int[] globalCols = new int[] {  0, 1, 0, -1 };
-    Queue<Coordinates> queue; 
-
-    public int RottenOrangesHelper(int[][] A)
-    {
-        int minTime = 0;
-        queue = new LinkedList<>();
-
-        int[][] timeToRotten = GenerateRottenMatrix(A);
 
         // Keep poping nodes and processing until Queue is empty.
         // Answer should be the max number of BFS.
         // if all component have at least 1 rotten orange Otherwise -1.
         while(!queue.isEmpty())
         {
-            Coordinates poppedNode = queue.remove();
+            int[] curr = queue.remove();
+            int r = curr[0];
+            int c = curr[1];
+            int timeInMinutes = curr[2];
 
-             // Traverse the neighbour nodes of the popped element.
-            for (int i = 0; i < 4; i++)
+            // We are 100% sure the last cell will give the last time to rotten
+            // In BFS, we are finding the further fresh orange from the rotten orange, 
+            // Hence the last level is the max time it will take.
+            ans = timeInMinutes;
+
+            // Check all valid neighbouring oranges
+            // S.C.: is O(1) as we are calling for every row and col and it will only go in 4 directions
+            for(int[] neighbour : GetAllValidNeighbours(A, r, c))
             {
-                int nr = poppedNode.X + globalRows[i];
-                int nc = poppedNode.Y + globalCols[i];
+                int nr = neighbour[0];
+                int nc = neighbour[1];
 
-                // Things to keep in mind :
-                // 1. check boundary conditions.
-                // 2. Nodes should be unvisited. ( i.e. visited[i][j] == INT_MIN)
-                // 3. Cell should contain an Orange
-                if(nr >= 0 && nr < A.length && nc >= 0 && nc < A[0].length 
-                    && A[nr][nc] == 1)
-                {
-                    // Get the Prev cell value of the popped cell X&Y and add + 1 time.
-                    timeToRotten[nr][nc] = timeToRotten[poppedNode.X][poppedNode.Y] + 1;
+                // Make this new cell with new coordinates as rotten.
+                A[nr][nc] = ROTTEN;
 
-                    // Update the time
-                    minTime = timeToRotten[nr][nc];
-
-                    // Make this cell with new coordinates as rotten.
-                    A[nr][nc] = 2;
-
-                    // Push the new coord. neighbour after processing the Queue.
-                    queue.add(new Coordinates(nr, nc));
-                }
+                // Push the new coord. neighbour after processing the Queue.
+                queue.add(new int[] {nr, nc, timeInMinutes + 1});
             }
         }
 
         // Iterate given question input to see if there is any 1 present.
         // If 1 is present then return -1, else, return the min time.
-        for (int i = 0; i < A.length; i++)
+        for (int i = 0; i < R; i++)
         {
-            for (int j = 0; j < A[0].length; j++)
+            for (int j = 0; j < C; j++)
             {
-                if (A[i][j] == 1)
+                if (A[i][j] == FRESH)
                 {
                     return -1;
                 }
-
-                //minTime = Math.Max(minTime, timeToRotten[i, j]);
             }
         }
 
-        return minTime;
+        return ans;
+
     }
 
-    public int[][] GenerateRottenMatrix(int[][] A)
+    public List<int[]> GetAllValidNeighbours(int[][] A, int currRow, int currCol)
     {
-        int[][] timeToRotten = new int[A.length][A[0].length];
+        List<int[]> neighbour = new ArrayList<>();
 
-        // Push all the rotten oranges for the BFS and create timeToRotten matrix.
-        for(int i = 0; i < A.length; i++)
+        for(int i = 0; i < 4; i++)
         {
-            for(int j = 0; j < A[0].length; j++)
-            {
-                if(A[i][j] == 2)
-                {
-                    // Push all the rotten oranges for the BFS.
-                    queue.add(new Coordinates(i, j));
+            int nr = currRow + DIRx[i];
+            int nc = currCol + DIRy[i];
 
-                    // initialise the rotten oranges as 0 (0 time , as they are already rotten).
-                    timeToRotten[i][j] = 0;
-                }
-                else
-                {
-                    timeToRotten[i][j] = Integer.MIN_VALUE;
-                }
+            if(nr >= 0 && nr < A.length && nc >= 0 && nc < A[0].length 
+               && A[nr][nc] == FRESH)
+            {
+                neighbour.add(new int[]{nr, nc});
             }
         }
 
-        return timeToRotten;
+        return neighbour;
     }
+    
 }
