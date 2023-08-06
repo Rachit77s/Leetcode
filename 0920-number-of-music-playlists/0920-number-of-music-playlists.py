b27001f1a20536591bb592bd48bd2d55eac16143
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-08-06 14:13:47 +0530

    Time: 50 ms (91.73%), Space: 16.6 MB (60.90%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.py
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.py
@@ -1,22 +1,11 @@
 
 class Solution:
-    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
-        MOD = 1_000_000_007
-        dp = [[-1 for _ in range(n + 1)] for _ in range(goal + 1)]
-
-        def number_of_playlists(i, j):
-            # Base cases
-            if i == 0 and j == 0:
-                return 1
-            if i == 0 or j == 0:
-                return 0
-            if dp[i][j] != -1:
-                return dp[i][j]
-            # DP transition: add a new song or replay an old one
-            dp[i][j] = (number_of_playlists(i - 1, j - 1) * (n - j + 1)) % MOD
-            if j > k:
-                dp[i][j] += (number_of_playlists(i - 1, j) * (j - k)) % MOD
-                dp[i][j] %= MOD
-            return dp[i][j]
-
-        return number_of_playlists(goal, n)
+    def numMusicPlaylists(self, N, L, K):
+        dp = [[0 for i in range(L + 1)] for j in range(N + 1)]
+        for i in range(K + 1, N + 1):
+            for j in range(i, L + 1):
+                if i == j or i == K + 1:
+                    dp[i][j] = math.factorial(i)
+                else:
+                    dp[i][j] = dp[i - 1][j - 1] * i + dp[i][j - 1] * (i - K)
+        return dp[N][L] % (10**9 + 7)
