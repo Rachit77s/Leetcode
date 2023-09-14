6f88869271e4f907311cf9ab48a403dce9309b62
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-09-14 19:55:46 +0530

    Time: 1 ms (38.83%), Space: 43 MB (62.52%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -11,7 +11,106 @@
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         
-        return ReverseInKGroup(head, k);
+        return ReverseInKGroupIterative(head, k);
+    }
+    
+    // Subham: https://www.scaler.com/help_requests/827697/
+    public ListNode ReverseInKGroupIterative(ListNode head, int k) 
+    {
+        /*
+            Idea is to save the 1st element of Kth group in a list,
+            while saving break the links as well
+            Afterwards reverse the list having LinkedList element
+            Finally join the reversed elements
+        
+        */
+        if(head == null || head.next == null || k == 1)
+            return head;
+        
+        ArrayList<ListNode> list = new ArrayList<>();
+        list.add(head);
+        
+        ListNode prev = null;
+        ListNode curr = head;
+        int index = 0;
+        
+        // Iterate and store every 1st element in the list
+        // Ex: 1 3 5
+        while(curr != null)
+        {
+            if(prev != null && index % k == 0)
+            {
+                // Break the link
+                prev.next = null;
+                list.add(curr);
+            }
+            
+            prev = curr;
+            curr = curr.next;
+            index += 1;
+        }
+        
+        // Reverse and update the reversed node as new head
+        for(int i = 0; i < list.size(); i++)
+        {
+            ListNode ll = list.get(i);
+            
+            // If the length is K then only reverse
+            if(GetLength(ll) == k)
+            {
+                ListNode newHead = Reverse(ll);
+                list.set(i, newHead);
+            }     
+        }
+        
+        // Join the LL's
+        for(int i = 0; i < list.size() - 1; i++)
+        {
+            ListNode prevLlEndNode = list.get(i);
+            
+            // Get to the end node of the curr LL
+            while(prevLlEndNode.next != null)
+                prevLlEndNode = prevLlEndNode.next;
+            
+            // Make the link
+            prevLlEndNode.next = list.get(i+1);
+        }
+            
+        return list.get(0);
+    }
+    
+    public ListNode Reverse(ListNode head) 
+    {
+        if(head == null || head.next == null)
+            return head;
+        
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = null;
+        
+        while(curr != null)
+        {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+    }
+    
+    public int GetLength(ListNode head) 
+    {
+        int length = 0;   
+        ListNode curr = head;
+        
+        while(curr != null)
+        {
+            curr = curr.next;
+            length++;
+        }
+        
+        return length;
     }
     
     // https://www.youtube.com/watch?v=znQ8wJxnRao&ab_channel=CodeLibrary-byYogesh%26Shailesh
