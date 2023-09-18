96bb6bc4d3f119f3f5dffd7db1318162766e795f
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-09-18 18:37:26 +0530

    Time: 1 ms (99.31%), Space: 44.4 MB (86.29%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,15 +1,26 @@
-class Solution:
-    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        m, n = len(mat), len(mat[0])
-        result = {}
-        for j in range(n):
-            for i in range(m):
-                if mat[i][j] or i in result: continue
-                result[i] = True
-                k -= 1
-                if not k: return result.keys()
-        for i in range(m):
-            if i not in result:
-                result[i] = True
-                k -= 1
-                if not k: return result.keys()
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+        int pos=0;
+        for (int[] row : mat)
+        {
+            int lo = 0, hi = row.length; 
+            
+            while(lo < hi)
+            {
+                int mid = (lo + hi) / 2;
+                if (row[mid] != 0) lo = mid + 1;
+                else hi = mid;
+            }
+            
+            q.add(new int[]{lo, pos++});
+        }
+        
+        int[] output = new int[k];
+        for(int i = 0; i < k; i++)  
+            output[i] = q.remove()[1];
+        
+        return output;
+    }
+}
