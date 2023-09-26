70edb4419022cde838e6ec425620b34576130872
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-09-26 20:19:28 +0530

    Time: 8 ms (99.50%), Space: 13.4 MB (78.61%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def removeDuplicateLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        last_occ = {c: i for i, c in enumerate(s)}
+        stack = ["!"]
+        Visited = set()
+        
+        for i, symbol in enumerate(s):
+            if symbol in Visited: continue
+            
+            while (symbol < stack[-1] and last_occ[stack[-1]] > i):
+                Visited.remove(stack.pop())
+           
+            stack.append(symbol)
+            Visited.add(symbol)        
+        return "".join(stack)[1:]
