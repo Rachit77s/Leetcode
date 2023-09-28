0dc40e2367be6cb49d0687add3e6f96db4d17830
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-09-28 23:46:40 +0530

    Time: 1 ms (90.29%), Space: 44.6 MB (38.46%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,40 +1,89 @@
 class Solution {
     public void setZeroes(int[][] A) {
         
+        // Alisha: https://www.youtube.com/watch?v=djt9mwch-Xo
         List < Integer > row = new ArrayList < > ();
         List < Integer > col = new ArrayList < > ();
 
         int n = A.length;
         int m = A[0].length;
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
         
-        for (int i = 0; i < n; i++) {
-          for (int j = 0; j < m; j++) 
-          {
-            if (A[i][j] == 0) {
-              row.add(i);
-              col.add(j);
+        
+        // Check if 1st row is zero
+        for(int c = 0; c < m; c++)
+        {
+            if(A[0][c] == 0)
+                firstRowZero = true;
+        }
+        
+        // Check if 1st col is zero
+        for(int r = 0; r < n; r++)
+        {
+            if(A[r][0] == 0)
+                firstColZero = true;
+        }
+        
+        // Iterate over the inner 3x3 matrix, and assign 
+        // 0th row and 0th col as 0 if we come across a 0
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 1; j < m; j++)
+            {
+                if(A[i][j] == 0)
+                {
+                    // Mark corresponding 0th row and 0th col as 0
+                    A[0][j] = 0;
+                    A[i][0] = 0;
+                }
             }
-          }
         }
-
-        for (int r = 0; r < row.size(); r++) {
-          int rowIdx = row.get(r);
-
-          // Iterate whole column
-          for (int c = 0; c < m; c++)
-            A[rowIdx][c] = 0;
-        }
-
-        for (int c = 0; c < col.size(); c++) {
-          int colIdx = col.get(c);
-
-          // Iterate whole row
-          for (int r = 0; r < n; r++)
-            A[r][colIdx] = 0;
-        }
-
-        // return A;
         
+        
+        // Mark entire row as 0 in the inner matrix
+        for(int j = 1; j < m; j++)
+        {
+            // Check if the curr col is 0, then mark its subsequent rows as 0
+            if(A[0][j] == 0)
+            {
+                for(int i = 1; i < n; i++)
+                {
+                    A[i][j] = 0;
+                }
+            }
+        }
+        
+        // Mark entire col as 0 in the inner matrix
+        for(int i = 1; i < n; i++)
+        {
+            // Check if the curr row is 0, then mark its subsequent col as 0
+            if(A[i][0] == 0)
+            {
+                for(int j = 1; j < m; j++)
+                {
+                    A[i][j] = 0;
+                }
+            }
+        }
+        
+        // Mark first row as 0
+        if(firstRowZero)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                A[0][j] = 0;
+            }
+        }
+        
+        // Mark first col as 0
+        if(firstColZero)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                A[i][0] = 0;
+            }
+        }
     }
 }
 
