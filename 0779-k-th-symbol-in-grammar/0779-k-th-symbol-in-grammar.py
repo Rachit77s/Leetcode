d47c654a9fdeeb524c2efea0e8beb4d042c9296c
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-10-25 15:27:35 +0530

    Time: 40 ms (46.39%), Space: 16.3 MB (17.91%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -0,0 +1,26 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
+
+        # We assume the symbol at the target position is '1'.
+        symbol = 1
+
+        for curr_row in range(n, 1, -1):
+            total_elements = 2 ** (curr_row - 1)
+            half_elements = total_elements // 2
+
+            # If 'k' lies in the right half symbol, then we flip over to the respective left half symbol.
+            if k > half_elements:
+                # Flip the symbol.
+                symbol = 1 - symbol
+                # Change the position after flipping.
+                k -= half_elements
+
+        # We reached the 1st row; if the symbol is not '0', we started with the wrong symbol.
+        if symbol != 0:
+            # Thus, the start symbol was '0', not '1'.
+            return 0
+
+        # The start symbol was indeed what we started with, a '1'.
+        return 1
