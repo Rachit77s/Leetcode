3d608365f79ca78b2f9086c3aa38a2d11e6d2205
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2023-11-14 22:20:38 +0530

    Time: 47 ms (36.98%), Space: 128.4 MB (6.24%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,112 +1,127 @@
-import java.util.Hashtable;
-
-
-public class LRUCache {
-
-class DLinkedNode {
-  int key;
-  int value;
-  DLinkedNode pre;
-  DLinkedNode post;
-}
-
-/**
- * Always add the new node right after head;
- */
-private void addNode(DLinkedNode node) {
+class LRUCache {
     
-  node.pre = head;
-  node.post = head.post;
+    // Step 1 : Create Doubly LinkedList class
+    class DoublyLLNode
+    {
+        public int key;
+        public int value;
 
-  head.post.pre = node;
-  head.post = node;
-}
+        // Reference Pointers
+        public DoublyLLNode prev;
+        public DoublyLLNode next;
 
-/**
- * Remove an existing node from the linked list.
- */
-private void removeNode(DLinkedNode node){
-  DLinkedNode pre = node.pre;
-  DLinkedNode post = node.post;
-
-  pre.post = post;
-  post.pre = pre;
-}
-
-/**
- * Move certain node in between to the head.
- */
-private void moveToHead(DLinkedNode node){
-  this.removeNode(node);
-  this.addNode(node);
-}
-
-// pop the current tail. 
-private DLinkedNode popTail(){
-  DLinkedNode res = tail.pre;
-  this.removeNode(res);
-  return res;
-}
-
-private Hashtable<Integer, DLinkedNode> 
-  cache = new Hashtable<Integer, DLinkedNode>();
-private int count;
-private int capacity;
-private DLinkedNode head, tail;
-
-public LRUCache(int capacity) {
-  this.count = 0;
-  this.capacity = capacity;
-
-  head = new DLinkedNode();
-  head.pre = null;
-
-  tail = new DLinkedNode();
-  tail.post = null;
-
-  head.post = tail;
-  tail.pre = head;
-}
-
-public int get(int key) {
-
-  DLinkedNode node = cache.get(key);
-  if(node == null){
-    return -1; // should raise exception here.
-  }
-
-  // move the accessed node to the head;
-  this.moveToHead(node);
-
-  return node.value;
-}
-
-
-public void put(int key, int value) {
-  DLinkedNode node = cache.get(key);
-
-  if(node == null){
-
-    DLinkedNode newNode = new DLinkedNode();
-    newNode.key = key;
-    newNode.value = value;
-
-    this.cache.put(key, newNode);
-    this.addNode(newNode);
-
-    ++count;
-
-    if(count > capacity){
-      // pop the tail
-      DLinkedNode tail = this.popTail();
-      this.cache.remove(tail.key);
-      --count;
+        public DoublyLLNode(int key, int value)
+        {
+            this.key = key;
+            this.value = value;
+        }
     }
-  }else{
-    // update the value.
-    node.value = value;
-    this.moveToHead(node);
-  }
+    
+    // Step 2: Create the LRUCache with 2 dummy nodes(head and tail), and a HashMap
+    // 2 Doubly LL, Head and Tail Dummy Nodes/Pointers
+    DoublyLLNode head = new DoublyLLNode(0, 0);
+    DoublyLLNode tail = new DoublyLLNode(0, 0);
+
+    HashMap<Integer, DoublyLLNode> map = new HashMap<Integer, DoublyLLNode>();
+    int capacity;
+
+    public LRUCache(int capacity) {
+        
+        this.capacity = capacity;
+        
+        // Attach head and tail
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    // Step 3: GET method
+    // Get Method: Find node, if found -> delete node and then insert node.
+    // Find the node address in the HashMap, if found, then remove it from its current position and add it to the head of the Doubly LL
+    // If Map has the key, then get the node address via the key, and this node becomes the recently used node,
+    // so remove it from its prev position and insert it just after the head.
+    public int get(int key) {
+        
+        if (map.containsKey(key))
+        {
+            // Get the Node's reference pointer
+            DoublyLLNode node = map.get(key);
+
+            // Now, this key becomes the LRU, therefore, perform 2 steps:
+            // a) Remove the node from its current position
+            // b) Insert the node at the head of the Doubly LL
+            remove(node);
+            insert(node); // This is moveToHead function
+            return node.value;
+        }
+
+        return -1;
+    }
+    
+    // Step 4: remove Method, Remove the given Node, and link its prev and next node in O(1)
+    public void remove(DoublyLLNode node)
+    {
+        // IMP: First Remove the node from Map
+        map.remove(node.key);
+
+        // Unlink the Node from other Nodes i.e. from its prev and next node
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+
+        /*
+        // Clean way to remove the node from the Doubly LL
+        // Get the prev and next nodes of the node to be removed
+        DoublyLLNode prevNode = node.prev;
+        DoublyLLNode nextNode = node.next;
+
+        // Now, remove the node from its current position
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+        */
+    }
+
+    // Step 5: Create a function to add a node to the head of the Doubly LL
+    // InsertNode Method, just after the dummy Head node in O(1) time
+    // Move node to the head i.e. moveToHead()
+    public void insert(DoublyLLNode node)
+    {
+        // IMP: First, Directly store the node in the map
+        map.put(node.key, node);
+
+        // Insert the node just after the dummy head node
+        DoublyLLNode tempNode = head.next;
+        head.next = node;
+        node.prev = head;
+        tempNode.prev = node;
+        node.next = tempNode;
+    }
+    
+    // Step 6: PUT Method
+    // Put Method: If the key is already present, then update the value and move the node to the head of the Doubly LL
+    // If the map contains node, remove the node since the same node with a new value came.
+    // If capacity becomes overflow, remove the node just before the tail node i.e. LRU node.
+    public void put(int key, int value) {
+        // IMP:  1st check does the LRUCache contains this key or not
+        if(map.containsKey(key))
+        {
+            // Get the Node's reference pointer
+            DoublyLLNode node = map.get(key);
+
+            // Remove the node because the same node with updated value has come up, we need to insert its new value.
+            remove(node);
+        }
+
+        // If the size of LRUCache got full or reached its capacity, remove the node which is just prev to the tail.
+        if(map.size() == capacity)
+            remove(tail.prev);
+
+        // At the last, insert the node to the head of the Doubly LL
+        insert(new DoublyLLNode(key, value));
+    }
 }
 
-}
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
