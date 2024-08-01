606053424a0e1459f3526dc3c8c0d7d6cc90a7dc
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2024-08-01 22:39:07 +0530

    Time: 47 ms (36.11%), Space: 16.6 MB (50.49%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.py
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -1,7 +1,15 @@
 class Solution:
     def decrypt(self, A: List[int], k: int) -> List[int]:
         
-        # 5 7 1 4 - 5 7 1 4
+        """
+            A: 2 4 9 3
+            Rev A: 3 9 4 2
+            A+A:  3 9 4 2  3 9 4 2
+                  13 6 5 12
+            Rev of O/P: 12 5 6 13
+         
+        """
+        
         
         if k == 0:
             return [0 for _ in A]
@@ -9,30 +17,32 @@ class Solution:
         if k > 0:
             ans = []
             n = len(A)
-            
+            A = A + A
+
             for i in range(n):
                 total = 0
                 
                 for j in range(1, k+1):
-                    total += A[(i+j) % n] 
+                    total += A[(i+j)] 
                     
                 ans.append(total)
                 
             return ans
-        
+
         if k < 0:
             k = abs(k)
             ans = []
             n = len(A)
-            
+            A = list(reversed(A))
+            A = A + A
+            #print(A)
+                
             for i in range(n):
                 total = 0
-                
+
                 for j in range(1, k+1):
-                    total += A[(i-j) % n] 
-                    
+                    total += A[(i+j)] 
+
                 ans.append(total)
-                
-            return ans
-                    
-            
+
+            return list(reversed(ans))
