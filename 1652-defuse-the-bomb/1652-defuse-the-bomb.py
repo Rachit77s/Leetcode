09aef04d56480795a36bcc8945f10767165b8087
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2024-08-03 20:25:16 +0530

    Time: 40 ms (73.08%), Space: 16.6 MB (21.20%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.py
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -7,42 +7,30 @@ class Solution:
             A+A:  3 9 4 2  3 9 4 2
                   13 6 5 12
             Rev of O/P: 12 5 6 13
+            
+            
+            
+          5  7  1  4    5  7  1  4
+          12 10 16 13   12 10 16 13
          
         """
         
         
-        if k == 0:
-            return [0 for _ in A]
+        n = len(A)
+        K = abs(k)
         
-        if k > 0:
-            ans = []
-            n = len(A)
-            A = A + A
-
-            for i in range(n):
-                total = 0
-                
-                for j in range(1, k+1):
-                    total += A[(i+j)] 
-                    
-                ans.append(total)
-                
-            return ans
-
         if k < 0:
-            k = abs(k)
-            ans = []
-            n = len(A)
-            A = list(reversed(A))
-            A = A + A
-            #print(A)
-                
-            for i in range(n):
-                total = 0
-
-                for j in range(1, k+1):
-                    total += A[(i+j)] 
-
-                ans.append(total)
-
-            return list(reversed(ans))
+            A.reverse()
+            
+        A = A + A
+        P = list(accumulate(A))
+        
+        ans = []
+        
+        for i in range(n):
+            ans.append(P[i+K] - P[i])
+            
+        if k < 0:
+            ans.reverse()
+            
+        return ans
