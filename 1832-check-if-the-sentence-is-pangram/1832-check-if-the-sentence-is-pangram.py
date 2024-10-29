f27007ff4a2fd7341cf6f71cc6bb884d848b71a1
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2024-10-29 12:24:36 +0530

    Time: 0 ms (100.00%), Space: 16.6 MB (42.37%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,16 +1,11 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        dictOfChar = defaultdict(int)
+        seen = set()
         
-        for i in range(97, 123):  # ASCII values of 'a' to 'z' are 97 to 122
-            dictOfChar[chr(i)] = 1
-            
-        dict2 = defaultdict(int)
         for ch in sentence:
-            dict2[ch] += 1
+            seen.add(ch)
             
-        for key, value in dictOfChar.items():
-            if dict2[key] == 0:
-                return False
-            
-        return True
+        if len(seen) == 26:
+            return True
+        
+        return False
