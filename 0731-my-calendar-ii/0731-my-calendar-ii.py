95dfdca9e1c714c4a007534d36fe5f2c0d198882
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2024-11-02 19:41:19 +0530

    Time: 1407 ms (15.48%), Space: 18.2 MB (5.36%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -1,24 +1,34 @@
+from sortedcontainers import SortedDict
 class MyCalendarTwo:
-
+    
+    # Alisha : https://www.youtube.com/watch?v=_7B_HzJUE6E
+    
     def __init__(self):
-        self.nonoverlapping = []
-        self.overlapping = [] # contains 2 overlapped events
+        self.events = SortedDict()
+        # The maximum number of overlapped bookings allowed.
+        self.maxOverlap = 2
 
-    def book(self, startTime: int, endTime: int) -> bool:
+    def book(self, start: int, end: int) -> bool:
         
-        # check for triple booking as overlapping already has 2 overlapping
-        for s, e in self.overlapping:
-            if endTime > s and e > startTime:
+        # add the current event in sorted dictionary
+        self.events[start] = self.events.get(start, 0) + 1
+        self.events[end] = self.events.get(end, 0) - 1
+        
+        currOverlap = 0
+
+        # Calculate the prefix sum of bookings.
+        for count in self.events.values():
+            currOverlap += count
+            
+            if currOverlap > self.maxOverlap:
+                # remove the added event & return false
+                self.events[start] -= 1
+                self.events[end] += 1
                 return False
+                
         
-        for s, e in self.nonoverlapping:
-            if endTime > s and e > startTime:
-                self.overlapping.append((max(s,startTime), min(e,endTime)))
-                
-        self.nonoverlapping.append((startTime, endTime))
         return True
-                
-                
+        
 # Your MyCalendarTwo object will be instantiated and called as such:
 # obj = MyCalendarTwo()
 # param_1 = obj.book(startTime,endTime)
