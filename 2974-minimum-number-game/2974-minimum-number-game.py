8aa6fd52160b28553b5f686501f302bd72e4698c
Rachit Srivastava <32384410+Rachit77s@users.noreply.github.com>
2024-11-05 17:21:58 +0530

    Time: 3 ms (47.40%), Space: 16.6 MB (21.05%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.py
+++ 2974-minimum-number-game/2974-minimum-number-game.py
@@ -1,11 +1,14 @@
+import heapq
+
 class Solution:
     def numberGame(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        nums.sort()
-        
-        for i in range(0, n, 2):
-            if i + 1 < n:
-                nums[i], nums[i + 1] = nums[i + 1], nums[i]
-                
-        return nums
-        
+        arr = []
+        heapq.heapify(nums)  # Convert nums into a heap in-place
+
+        while nums:
+            alice = heapq.heappop(nums)
+            bob = heapq.heappop(nums)
+            arr.append(bob)
+            arr.append(alice)
+
+        return arr
